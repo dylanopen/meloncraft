2dd269b14bbd5c561bddf229ffb5e1db817f5585
dylancode <opendylan@proton.me>
2026-01-01 21:52:56 +0000

    working packet reader finally hopefully - todo: refactor ConnectionState

--- crates/meloncraft_network/src/tcp_reader.rs
+++ crates/meloncraft_network/src/tcp_reader.rs
@@ -23,7 +23,7 @@ const VARINT_CONTINUE_BIT: u8 = 0b10000000;
 pub fn handle_client(stream: TcpStream, entity: Entity) {
     let mut stream = stream.try_clone().unwrap();
     stream
-        .set_read_timeout(Some(Duration::from_millis(1000)))
+        .set_read_timeout(Some(Duration::from_millis(2000)))
         .unwrap();
     let address = stream.peer_addr().unwrap();
     let mut buf_reader = BufReader::new(&mut stream);
@@ -34,51 +34,21 @@ pub fn handle_client(stream: TcpStream, entity: Entity) {
             if buf_reader.read_exact(&mut single_byte_buf).is_err() {
                 break; // no more packets to read
             }
+            length_bytes.push(single_byte_buf[0]);
             if single_byte_buf[0] & VARINT_CONTINUE_BIT == 0 {
                 break; // no more data in varint to read
             }
-            length_bytes.push(single_byte_buf[0]);
         }
-        dbg!(&length_bytes);
         let length = deserialize::varint(&mut length_bytes).unwrap().0;
         let mut raw_packet: Vec<u8> = vec![0; length as usize];
-        println!("receiving packet from client {entity}");
-        buf_reader.read_to_end(&mut raw_packet).unwrap();
-        dbg!(&raw_packet);
-        // client sent incomplete packets, disconnect them
-
+        if buf_reader.read_exact(&mut raw_packet).is_err() {
+            println!("Client {entity} disconnected due to failing to stream packets");
+            break;
+        }
         if raw_packet.is_empty() {
-            //break;
+            break;
         }
 
-        /*
-        let mut packet_len = [0; 3];
-        let Ok(_) = stream.peek(&mut packet_len) else {
-            sleep(Duration::from_millis(1));
-            dbg!("peeking");
-            continue;
-        };
-        let len = deserialize::varint(&mut packet_len.to_vec()).unwrap();
-        if len == 0 {
-            continue; // client disconnected
-        }
-        let mut raw_packet = vec![0; (len + 3) as usize];
-        if stream.read_exact(&mut raw_packet).is_err() {
-            // no more packets to read for now
-            sleep(Duration::from_millis(1));
-            continue;
-        };
-        if raw_packet.is_empty() {
-            sleep(Duration::from_millis(1));
-            println!("Client {entity} disconnected");
-            continue;
-        }
-        println!("packet sent from client {entity}");
-         */
-
-        let (length, length_bytes) = deserialize::varint(&mut raw_packet).unwrap();
-        dbg!(length, length_bytes);
-        buf_reader.consume((length + length_bytes) as usize);
         dbg!(&raw_packet);
 
         let packet_id = deserialize::varint(&mut raw_packet).unwrap();
