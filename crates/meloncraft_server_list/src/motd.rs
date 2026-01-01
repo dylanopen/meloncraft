23617f2ec22d64f211bd267e22ef15e4bbba599e
dylancode <opendylan@proton.me>
2026-01-01 01:06:42 +0000

    create Motd resource

---
+++ crates/meloncraft_server_list/src/motd.rs
@@ -0,0 +1,18 @@
+use std::ops::{Deref, DerefMut};
+use bevy::ecs::resource::Resource;
+
+#[derive(Resource)]
+pub struct Motd(String);
+
+impl Deref for Motd {
+    type Target = String;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Motd {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
