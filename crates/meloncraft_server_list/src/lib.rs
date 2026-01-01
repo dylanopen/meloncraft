0c255186fee3c66edac771667a69bd0ae3709727
dylancode <opendylan@proton.me>
2026-01-01 01:10:26 +0000

    create MeloncraftServerListPlugin

--- crates/meloncraft_server_list/src/lib.rs
+++ crates/meloncraft_server_list/src/lib.rs
@@ -1,1 +1,11 @@
+use bevy::app::{App, Plugin};
+
 pub mod motd;
+
+pub struct MeloncraftServerListPlugin;
+
+impl Plugin for MeloncraftServerListPlugin {
+    fn build(&self, app: &mut App) {
+
+    }
+}
