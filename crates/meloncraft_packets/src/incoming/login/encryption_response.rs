acc94a210dade8d871e0d37e22c200b2eebbccb6
dylancode <opendylan@proton.me>
2026-01-02 17:29:46 +0000

    edit EncryptionResponse to use Vec<Byte> instead of Vec<i8>

--- crates/meloncraft_packets/src/incoming/login/encryption_response.rs
+++ crates/meloncraft_packets/src/incoming/login/encryption_response.rs
@@ -3,13 +3,13 @@ use bevy::ecs::message::Message;
 use bevy::prelude::Entity;
 use meloncraft_client::connection_state::ConnectionState;
 use meloncraft_network::packet::IncomingNetworkPacket;
-use meloncraft_protocol_types::{PrefixedArray, ProtocolBuffer};
+use meloncraft_protocol_types::{Byte, PrefixedArray, ProtocolBuffer};
 
 #[derive(Message, Debug, Clone)]
 pub struct EncryptionResponse {
     pub client: Entity,
-    pub shared_secret: Vec<i8>, // bytes are stored as i8s in the
-    pub verify_token: Vec<i8>,  // mc protocol for some reason
+    pub shared_secret: Vec<Byte>, // bytes are stored as i8s in the
+    pub verify_token: Vec<Byte>,  // mc protocol for some reason
 }
 
 impl IncomingPacket for EncryptionResponse {
@@ -23,9 +23,9 @@ impl IncomingPacket for EncryptionResponse {
 
     fn parse(incoming: &IncomingNetworkPacket) -> Option<Self> {
         let mut incoming = incoming.clone();
-        let shared_secret: PrefixedArray<i8> = incoming.data.net_deserialize().unwrap();
+        let shared_secret: PrefixedArray<Byte> = incoming.data.net_deserialize().unwrap();
         let shared_secret = shared_secret.values;
-        let verify_token: PrefixedArray<i8> = incoming.data.net_deserialize().unwrap();
+        let verify_token: PrefixedArray<Byte> = incoming.data.net_deserialize().unwrap();
         let verify_token = verify_token.values;
         Some(Self {
             client: incoming.client,
