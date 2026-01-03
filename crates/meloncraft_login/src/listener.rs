45db776d6b301174bcf257fe508e3335a9616b0e
dylancode <opendylan@proton.me>
2026-01-03 18:15:07 +0000

    login: respond to LoginStart with LoginSuccess if in OfflineUnencrypted mode

---
+++ crates/meloncraft_login/src/listener.rs
@@ -0,0 +1,28 @@
+use crate::encryption::EncryptionMode;
+use bevy::prelude::{MessageReader, MessageWriter, Query, Res};
+use meloncraft_client::connection::ClientConnection;
+use meloncraft_client::connection_state::ConnectionState;
+use meloncraft_packets::incoming::login::LoginStart;
+use meloncraft_packets::outgoing::login::LoginSuccess;
+use meloncraft_player::GameProfile;
+
+pub fn login_offline_unencrypted_listener(
+    mut login_start_pr: MessageReader<LoginStart>,
+    encryption_mode: Res<EncryptionMode>,
+    mut client_connections: Query<&mut ClientConnection>,
+    mut login_success_pw: MessageWriter<LoginSuccess>,
+) {
+    if *encryption_mode != EncryptionMode::OfflineUnencrypted {
+        return; // we only handle offline-mode and unencrypted in this system
+    }
+    for packet in login_start_pr.read() {
+        login_success_pw.write(LoginSuccess {
+            client: packet.client,
+            game_profile: GameProfile {
+                username: packet.name.clone(),
+                uuid: packet.uuid.clone(),
+            },
+        });
+        client_connections.get_mut(packet.client).unwrap().state = ConnectionState::Configuration;
+    }
+}
