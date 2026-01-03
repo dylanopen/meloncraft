6a7a150005f7c74e976a236296b9c826566ab8ed
dylancode <opendylan@proton.me>
2026-01-03 15:47:26 +0000

    update example server_list_ping to disconnect with reason

--- examples/server_list_ping/src/main.rs
+++ examples/server_list_ping/src/main.rs
@@ -1,11 +1,13 @@
 use bevy::MinimalPlugins;
 use bevy::app::{App, ScheduleRunnerPlugin};
 use bevy::app::{PluginGroup, Update};
-use bevy::prelude::MessageReader;
+use bevy::prelude::{MessageReader, MessageWriter};
 use meloncraft::handshaking::MeloncraftHandshakingPlugin;
 use meloncraft::network::MeloncraftNetworkPlugin;
 use meloncraft::packets::MeloncraftPacketsPlugin;
 use meloncraft::packets::incoming::login::LoginStart;
+use meloncraft::packets::outgoing::login::Disconnect;
+use meloncraft::protocol_types::JsonText;
 use meloncraft::server_list::MeloncraftServerListPlugin;
 use meloncraft::server_list::max_players::MaxPlayers;
 use meloncraft::server_list::motd::Motd;
@@ -30,11 +32,19 @@ pub fn main() {
     app.run();
 }
 
-fn login_listener(mut pr: MessageReader<LoginStart>) {
+fn login_listener(mut pr: MessageReader<LoginStart>, mut pw: MessageWriter<Disconnect>) {
     for packet in pr.read() {
         println!(
             "{} tried to log-in! This example only adds serverlist functionality.",
             packet.name
         );
+        pw.write(Disconnect {
+            client: packet.client,
+            reason: JsonText {
+                data:
+                    "{text: \"This example only supports server list pings - no joining yet ;)\"}"
+                        .to_owned(),
+            },
+        });
     }
 }
