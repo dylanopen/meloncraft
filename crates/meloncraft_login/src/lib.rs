95d8173bb929a3508e94bbfb6fda7dd01d63d0c6
dylancode <opendylan@proton.me>
2026-01-03 18:14:45 +0000

    create MeloncraftLoginPlugin

--- crates/meloncraft_login/src/lib.rs
+++ crates/meloncraft_login/src/lib.rs
@@ -1,1 +1,18 @@
+mod encryption;
+mod listener;
+mod verify;
 
+pub use encryption::EncryptionMode;
+
+use crate::listener::login_offline_unencrypted_listener;
+use crate::verify::verify_encryption;
+use bevy::app::{App, Plugin, PostStartup, Update};
+
+pub struct MeloncraftLoginPlugin;
+
+impl Plugin for MeloncraftLoginPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PostStartup, verify_encryption);
+        app.add_systems(Update, login_offline_unencrypted_listener);
+    }
+}
