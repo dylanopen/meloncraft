08d03de1859331a5eba2f00a24fde4ff5e8b5ae2
dylancode <opendylan@proton.me>
2026-01-03 09:53:10 +0000

    create crate meloncraft_nbt

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub use meloncraft_client as client;
 pub use meloncraft_handshaking as handshaking;
+pub use meloncraft_nbt as nbt;
 pub use meloncraft_network as network;
 pub use meloncraft_packets as packets;
+pub use meloncraft_player as player;
 pub use meloncraft_protocol_types as protocol_types;
 pub use meloncraft_server_list as server_list;
