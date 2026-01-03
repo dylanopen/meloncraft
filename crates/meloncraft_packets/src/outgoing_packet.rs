9f12e3411933ae808e81b8a9a481f9131d464e9a
dylancode <opendylan@proton.me>
2026-01-03 21:05:44 +0000

    require Message, Debug and Clone traits for OutgoingPacket

--- crates/meloncraft_packets/src/outgoing_packet.rs
+++ crates/meloncraft_packets/src/outgoing_packet.rs
@@ -1,7 +1,9 @@
+use bevy::prelude::Message;
 use meloncraft_client::connection_state::ConnectionState;
 use meloncraft_network::packet::OutgoingNetworkPacket;
+use std::fmt::Debug;
 
-pub trait OutgoingPacket: Sized {
+pub trait OutgoingPacket: Sized + Message + Debug + Clone {
     fn id() -> i32;
     fn state() -> ConnectionState;
     fn serialize(&self) -> Option<OutgoingNetworkPacket>;
