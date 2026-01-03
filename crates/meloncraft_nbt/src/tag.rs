11d48dbb078ab99a49688d4c8eb57a7f45c20e5d
dylancode <opendylan@proton.me>
2026-01-03 10:16:46 +0000

    move NbtValue from mod tag to value

--- crates/meloncraft_nbt/src/tag.rs
+++ crates/meloncraft_nbt/src/tag.rs
@@ -1,32 +1,7 @@
+use crate::value::NbtValue;
+
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct NbtTag {
     pub key: String,
     pub value: NbtValue,
-}
-
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
-pub enum NbtValue {
-    Root,
-    U8(u8),
-    I16(i16),
-    I32(i32),
-    I64(i64),
-    F32(f32),
-    F64(f64),
-    ArrayU8(Vec<u8>),
-    ArrayI32(Vec<i32>),
-    ArrayI64(Vec<i64>),
-    String(String),
-    List(Vec<NbtValue>),
-    Compound(Vec<NbtTag>),
-}
-
-impl NbtValue {
-    pub fn get_compound_children(&self) -> Option<&Vec<NbtTag>> {
-        if let NbtValue::Compound(compound) = self {
-            Some(compound)
-        } else {
-            None
-        }
-    }
 }
