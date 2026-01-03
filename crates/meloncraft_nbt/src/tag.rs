4bcda3f57d61101f71a09967202b9e5a23a71e69
dylancode <opendylan@proton.me>
2026-01-03 10:23:27 +0000

    create NbtTag::new

--- crates/meloncraft_nbt/src/tag.rs
+++ crates/meloncraft_nbt/src/tag.rs
@@ -5,3 +5,9 @@ pub struct NbtTag {
     pub key: String,
     pub value: NbtValue,
 }
+
+impl NbtTag {
+    pub fn new(key: String, value: NbtValue) -> Self {
+        NbtTag { key, value }
+    }
+}
