1c402eb0ee1212319db84341f8402d00815651c0
dylancode <opendylan@proton.me>
2026-01-04 17:36:29 +0000

    create component Locale

--- crates/meloncraft_player/src/lib.rs
+++ crates/meloncraft_player/src/lib.rs
@@ -1,8 +1,10 @@
 mod game_profile;
+mod locale;
 mod username;
 mod uuid;
 
 pub use game_profile::GameProfile;
 pub use game_profile::GameProfileProperties;
+pub use locale::Locale;
 pub use username::Username;
 pub use uuid::Uuid;
