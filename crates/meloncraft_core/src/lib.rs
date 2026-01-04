46851144fe2554af2ef165b9514f2714ecfe80e2
dylancode <opendylan@proton.me>
2026-01-04 16:13:06 +0000

    create enum BuiltinPauseMenuLabel

--- crates/meloncraft_core/src/lib.rs
+++ crates/meloncraft_core/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod datapack;
 mod disconnect_report;
 mod identifier;
+mod pause_menu_label;
 
 pub use disconnect_report::DisconnectReport;
 pub use identifier::Identifier;
+pub use pause_menu_label::PauseMenuLabel;
---
+++ crates/meloncraft_core/src/pause_menu_label.rs
@@ -0,0 +1,13 @@
+#[derive(Debug, Clone)]
+pub enum BuiltinPauseMenuLabel {
+    BugReport,
+    CommunityGuidelines,
+    Support,
+    Status,
+    Feedback,
+    Community,
+    Website,
+    Forums,
+    News,
+    Announcements,
+}
