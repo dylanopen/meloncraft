007e29129217943bd57515086ce43e7ccdab6711
dylancode <opendylan@proton.me>
2026-01-04 17:38:07 +0000

    create component ViewDistance

--- crates/meloncraft_player/src/lib.rs
+++ crates/meloncraft_player/src/lib.rs
@@ -2,9 +2,11 @@ mod game_profile;
 mod locale;
 mod username;
 mod uuid;
+mod view_distance;
 
 pub use game_profile::GameProfile;
 pub use game_profile::GameProfileProperties;
 pub use locale::Locale;
 pub use username::Username;
 pub use uuid::Uuid;
+pub use view_distance::ViewDistance;
