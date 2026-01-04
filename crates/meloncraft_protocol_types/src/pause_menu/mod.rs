3ba482bcdb51861eb6c828cef32f82adeef824d6
dylancode <opendylan@proton.me>
2026-01-04 16:37:57 +0000

    impl ProtocolType for PauseMenuLink

--- crates/meloncraft_protocol_types/src/pause_menu/mod.rs
+++ crates/meloncraft_protocol_types/src/pause_menu/mod.rs
@@ -1,3 +1,4 @@
 mod builtin_pause_menu_label;
 mod custom_pause_menu_label;
 mod pause_menu_label;
+mod pause_menu_link;
---
+++ crates/meloncraft_protocol_types/src/pause_menu/pause_menu_link.rs
@@ -0,0 +1,16 @@
+use crate::ProtocolType;
+use meloncraft_core::pause_menu::PauseMenuLink;
+
+impl ProtocolType for PauseMenuLink {
+    fn net_serialize(&self) -> Vec<u8> {
+        let mut output = self.label.net_serialize();
+        output.extend(self.url.net_serialize());
+        output
+    }
+
+    fn net_deserialize(data: &mut Vec<u8>) -> Result<Self, ()> {
+        todo!(
+            "It seems unnecessary to implement deserialization, as it will be complicated due to varying types, and we will never receive this data, only send it."
+        );
+    }
+}
