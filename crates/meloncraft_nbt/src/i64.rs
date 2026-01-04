f609aa9c54fa3903c9b8b407dad219f9130da758
dylancode <opendylan@proton.me>
2026-01-04 11:08:45 +0000

    create NbtI64

---
+++ crates/meloncraft_nbt/src/i64.rs
@@ -0,0 +1,2 @@
+#[derive(Debug, Clone)]
+pub struct NbtI64(pub i64);
--- crates/meloncraft_nbt/src/lib.rs
+++ crates/meloncraft_nbt/src/lib.rs
@@ -3,6 +3,7 @@ mod value;
 
 mod i16;
 mod i32;
+mod i64;
 mod u8;
 
 pub use tag::NbtTag;
@@ -10,4 +11,5 @@ pub use value::NbtValue;
 
 pub use i16::NbtI16;
 pub use i32::NbtI32;
+pub use i64::NbtI64;
 pub use u8::NbtU8;
