f8f5284c94c1efed47e3629b32b1a6ae3d51283c
dylancode <opendylan@proton.me>
2026-01-04 16:22:43 +0000

    impl TryFrom<i32> for BuiltinPauseMenuLabel

--- crates/meloncraft_core/src/pause_menu.rs
+++ crates/meloncraft_core/src/pause_menu.rs
@@ -14,6 +14,25 @@ pub enum BuiltinPauseMenuLabel {
     Announcements,
 }
 
+impl TryFrom<i32> for BuiltinPauseMenuLabel {
+    type Error = ();
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(BuiltinPauseMenuLabel::BugReport),
+            1 => Ok(BuiltinPauseMenuLabel::CommunityGuidelines),
+            2 => Ok(BuiltinPauseMenuLabel::Support),
+            3 => Ok(BuiltinPauseMenuLabel::Status),
+            4 => Ok(BuiltinPauseMenuLabel::Feedback),
+            5 => Ok(BuiltinPauseMenuLabel::Community),
+            6 => Ok(BuiltinPauseMenuLabel::Website),
+            7 => Ok(BuiltinPauseMenuLabel::Forums),
+            8 => Ok(BuiltinPauseMenuLabel::News),
+            9 => Ok(BuiltinPauseMenuLabel::Announcements),
+            _ => Err(()),
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct CustomPauseMenuLabel(pub NbtText);
 
