5c90ee670a669594efbb6bdf4d49c7df5b0d1cbb
dylancode <opendylan@proton.me>
2026-01-04 11:09:24 +0000

    create NbtF32

--- crates/meloncraft_nbt/src/lib.rs
+++ crates/meloncraft_nbt/src/lib.rs
@@ -1,6 +1,7 @@
 mod tag;
 mod value;
 
+mod f32;
 mod i16;
 mod i32;
 mod i64;
@@ -13,3 +14,4 @@ pub use i16::NbtI16;
 pub use i32::NbtI32;
 pub use i64::NbtI64;
 pub use u8::NbtU8;
+pub use f32::NbtF32;
