da192c8f737bde54ea5764eeba77b2fa70c45503
dylancode <opensource@dylancode.dev>
2026-05-11 19:58:11 +0100

    create packet clientbound/play/PlayerInfoUpdate

--- crates/meloncraft_packets/src/clientbound/play/mod.rs
+++ crates/meloncraft_packets/src/clientbound/play/mod.rs
@@ -4,6 +4,9 @@ pub use login::Login;
 mod synchronize_player_position;
 pub use synchronize_player_position::SynchronizePlayerPosition;
 
+mod player_info_update;
+pub use player_info_update::PlayerInfoUpdate;
+
 pub fn register_packets(app: &mut bevy::app::App) {
     use crate::clientbound_messenger::fwd;
     use bevy::app::PostUpdate;
@@ -13,4 +16,7 @@ pub fn register_packets(app: &mut bevy::app::App) {
 
     app.add_message::<SynchronizePlayerPosition>();
     app.add_systems(PostUpdate, fwd::<SynchronizePlayerPosition>);
+
+    app.add_message::<PlayerInfoUpdate>();
+    app.add_systems(PostUpdate, fwd::<PlayerInfoUpdate>);
 }
---
+++ crates/meloncraft_packets/src/clientbound/play/player_info_update.rs
@@ -0,0 +1,47 @@
+use bevy::ecs::entity::Entity;
+use bevy::ecs::message::Message;
+use meloncraft_client::connection_state::ConnectionState;
+use meloncraft_network::packet::ClientboundNetworkPacket;
+use meloncraft_player::Uuid;
+use meloncraft_protocol_types::{PlayerAction, ProtocolType, VarInt};
+
+use crate::clientbound_packet::ClientboundPacket;
+
+#[derive(Message, Debug, Clone)]
+pub struct PlayerInfoUpdate {
+    pub client: Entity,
+    pub action_mask: u8,
+    pub players: Vec<(Uuid, Vec<PlayerAction>)>,
+}
+
+impl ClientboundPacket for PlayerInfoUpdate {
+    fn id() -> i32 {
+        0x44
+    }
+
+    fn state() -> ConnectionState {
+        ConnectionState::Play
+    }
+
+    fn serialize(&self) -> Option<ClientboundNetworkPacket> {
+        let mut data = Vec::new();
+        data.push(self.action_mask); // 0xFF for all actions
+        data.extend(VarInt(self.players.len() as i32).net_serialize());
+
+        for player in &self.players {
+            data.extend(player.0.net_serialize());
+            for action in &player.1 {
+                data.extend(action.net_serialize());
+            }
+        }
+
+        println!("PlayerInfoUpdate packet data: {:x?}", &data);
+
+        Some(meloncraft_network::packet::ClientboundNetworkPacket {
+            client: self.client,
+            id: Self::id(),
+            data,
+        })
+    }
+}
+
