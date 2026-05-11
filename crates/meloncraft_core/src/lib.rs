62ab0562216a59f457a655848ba97011b43d7b95
dylancode <opensource@dylancode.dev>
2026-05-11 20:19:34 +0100

    create WeatherIntensity

--- crates/meloncraft_core/src/lib.rs
+++ crates/meloncraft_core/src/lib.rs
@@ -5,9 +5,11 @@ pub mod pause_menu;
 pub mod resource_pack_load_result;
 pub mod gamemode;
 pub mod demo_event;
+pub mod weather_intensity;
 
 pub use disconnect_report::DisconnectReport;
 pub use identifier::Identifier;
 pub use resource_pack_load_result::ResourcePackLoadResult;
 pub use gamemode::GameMode;
 pub use demo_event::DemoEventType;
+pub use weather_intensity::WeatherIntensity;
---
+++ crates/meloncraft_core/src/weather_intensity.rs
@@ -0,0 +1,5 @@
+/// The intensity of the rain or thunder, as a value between 0.0 and 1.0.
+/// 0.0 means no rain/thunder, 1.0 means full intensity.
+#[derive(Debug, Clone)]
+pub struct WeatherIntensity(pub f32);
+
