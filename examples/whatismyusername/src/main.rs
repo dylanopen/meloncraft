9ca7d6b5bdad13b19275e23564d0b7ba0fc4ef9a
dylancode <opensource@dylancode.dev>
2026-05-12 09:19:35 +0100

    create example whatismyusername

---
+++ examples/whatismyusername/src/main.rs
@@ -0,0 +1,44 @@
+use bevy::MinimalPlugins;
+use bevy::app::{App, ScheduleRunnerPlugin};
+use bevy::app::{PluginGroup, Update};
+use bevy::prelude::{MessageReader, MessageWriter};
+use meloncraft::handshaking::MeloncraftHandshakingPlugin;
+use meloncraft::network::MeloncraftNetworkPlugin;
+use meloncraft::packets::MeloncraftPacketsPlugin;
+use meloncraft::packets::clientbound::login::Disconnect;
+use meloncraft::packets::serverbound::login::LoginStart;
+use meloncraft::server_list::MeloncraftServerListPlugin;
+use meloncraft::server_list::max_players::MaxPlayers;
+use meloncraft::server_list::motd::Motd;
+use meloncraft::server_list::online_players::OnlinePlayers;
+use meloncraft::text::JsonText;
+use std::time::Duration;
+
+pub fn main() {
+    let mut app = App::new();
+
+    app.insert_resource(Motd("Check your username by joining this server".to_owned()));
+    app.insert_resource(OnlinePlayers(987654321));
+    app.insert_resource(MaxPlayers(1234567890));
+
+    app.add_plugins(MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_millis(50))));
+    app.add_plugins(MeloncraftNetworkPlugin);
+    app.add_plugins(MeloncraftPacketsPlugin);
+    app.add_plugins(MeloncraftHandshakingPlugin);
+    app.add_plugins(MeloncraftServerListPlugin);
+
+    app.add_systems(Update, login_listener);
+
+    app.run();
+}
+
+fn login_listener(mut pr: MessageReader<LoginStart>, mut pw: MessageWriter<Disconnect>) {
+    for packet in pr.read() {
+        pw.write(Disconnect {
+            client: packet.client,
+            reason: JsonText {
+                data: format!("{{text: \"Your username is {} \"}}", packet.name)
+            },
+        });
+    }
+}
