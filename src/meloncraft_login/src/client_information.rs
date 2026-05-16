6326170173e53d5617d96b621ed912e7630690d0
dylancode <opensource@dylancode.dev>
2026-05-16 21:14:01 +0100

    login: send client information messages upon receiving

--- src/meloncraft_login/src/client_information.rs
+++ src/meloncraft_login/src/client_information.rs
@@ -1,22 +1,62 @@
-use bevy::prelude::{Commands, MessageReader};
+use bevy::prelude::{MessageReader, MessageWriter};
 use meloncraft_packets::ServerboundClientInformation;
+use crate::messages::{
+    ClientAllowPlayerListingsReceived, ClientChatColorsReceived, ClientChatModeReceived,
+    ClientDisplayedSkinPartsReceived, ClientEnableTextFilteringReceived, ClientLocaleReceived,
+    ClientMainHandReceived, ClientParticleRenderingModeReceived, ClientViewDistanceReceived,
+};
 
+#[expect(clippy::too_many_arguments, reason = "All args necessary, they're just message writers")]
 pub fn client_information_listener(
     mut login_acknowledged_pr: MessageReader<ServerboundClientInformation>,
-    mut commands: Commands,
+    mut locale_mw: MessageWriter<ClientLocaleReceived>,
+    mut view_distance_mw: MessageWriter<ClientViewDistanceReceived>,
+    mut chat_mode_mw: MessageWriter<ClientChatModeReceived>,
+    mut chat_colors_mw: MessageWriter<ClientChatColorsReceived>,
+    mut displayed_skin_parts_mw: MessageWriter<ClientDisplayedSkinPartsReceived>,
+    mut main_hand_mw: MessageWriter<ClientMainHandReceived>,
+    mut enable_text_filtering_mw: MessageWriter<ClientEnableTextFilteringReceived>,
+    mut allow_player_listings_mw: MessageWriter<ClientAllowPlayerListingsReceived>,
+    mut particle_rendering_mode_mw: MessageWriter<ClientParticleRenderingModeReceived>,
 ) {
     for packet in login_acknowledged_pr.read() {
-        let mut entity = commands.entity(packet.client);
-        entity.insert((
-            packet.locale.clone(),
-            packet.view_distance,
-            packet.chat_mode,
-            packet.chat_colors,
-            packet.displayed_skin_parts.clone(),
-            packet.main_hand,
-            packet.enable_text_filtering,
-            packet.allow_player_listings,
-            packet.particle_rendering_mode.clone(),
-        ));
+        let client = packet.client;
+
+        locale_mw.write(ClientLocaleReceived {
+            client,
+            locale: packet.locale.clone(),
+        });
+        view_distance_mw.write(ClientViewDistanceReceived {
+            client,
+            view_distance: packet.view_distance,
+        });
+        chat_mode_mw.write(ClientChatModeReceived {
+            client,
+            chat_mode: packet.chat_mode,
+        });
+        chat_colors_mw.write(ClientChatColorsReceived {
+            client,
+            chat_colors: packet.chat_colors,
+        });
+        displayed_skin_parts_mw.write(ClientDisplayedSkinPartsReceived {
+            client,
+            displayed_skin_parts: packet.displayed_skin_parts.clone(),
+        });
+        main_hand_mw.write(ClientMainHandReceived {
+            client,
+            main_hand: packet.main_hand,
+        });
+        enable_text_filtering_mw.write(ClientEnableTextFilteringReceived {
+            client,
+            enable_text_filtering: packet.enable_text_filtering,
+        });
+        allow_player_listings_mw.write(ClientAllowPlayerListingsReceived {
+            client,
+            allow_player_listings: packet.allow_player_listings,
+        });
+        particle_rendering_mode_mw.write(ClientParticleRenderingModeReceived {
+            client,
+            particle_rendering_mode: packet.particle_rendering_mode.clone(),
+        });
     }
 }
