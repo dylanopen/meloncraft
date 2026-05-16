44cc35f00968d82a62845fe0bb0b85c5a2b8a9fe
dylancode <opensource@dylancode.dev>
2026-05-16 22:32:12 +0100

    world_manager: SendChunk handler

--- src/meloncraft_world_manager/src/lib.rs
+++ src/meloncraft_world_manager/src/lib.rs
@@ -1,7 +1,8 @@
 pub mod messages;
 pub mod marker;
+mod sender;
 
-use bevy::app::{App, Plugin};
+use bevy::app::{App, Plugin, Update};
 use bevy::prelude::World;
 use crate::marker::Overworld;
 
@@ -10,5 +11,7 @@ pub struct MeloncraftWorldManagerPlugin;
 impl Plugin for MeloncraftWorldManagerPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Overworld(World::default()));
+
+        app.add_systems(Update, sender::send_chunk);
     }
 }
---
+++ src/meloncraft_world_manager/src/sender.rs
@@ -0,0 +1,28 @@
+use bevy::prelude::{MessageReader, MessageWriter};
+use crate::messages::SendChunk;
+use meloncraft_packets::ClientboundChunkData;
+use meloncraft_protocol_types::bitset::BitSet;
+use meloncraft_protocol_types::chunk_lighting::ChunkLighting;
+
+pub fn send_chunk(
+    mut send_chunk_mr: MessageReader<SendChunk>,
+    mut chunk_data_pw: MessageWriter<ClientboundChunkData>,
+) {
+    for send_chunk in send_chunk_mr.read() {
+        chunk_data_pw.write(ClientboundChunkData {
+            client: send_chunk.client,
+            chunk_x: send_chunk.chunk_x,
+            chunk_z: send_chunk.chunk_z,
+            data: send_chunk.chunk.to_chunk_sections(),
+            // Lighting is purely temporary:
+            light: ChunkLighting {
+                sky_mask: BitSet::with_capacity(26),
+                block_mask: BitSet::with_capacity(26),
+                empty_sky_mask: BitSet::with_capacity(26),
+                empty_block_mask: BitSet::with_capacity(26),
+                sky_data: vec![],
+                block_data: vec![],
+            },
+        });
+    }
+}
