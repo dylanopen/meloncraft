d1b911d3fe97d615eda4761ffb84f33f6a57e7c2
dylancode <opensource@dylancode.dev>
2026-05-16 22:16:34 +0100

    Overworld resource

--- src/meloncraft_world_manager/src/lib.rs
+++ src/meloncraft_world_manager/src/lib.rs
@@ -1,8 +1,13 @@
+pub mod marker;
+
 use bevy::app::{App, Plugin};
+use bevy::prelude::World;
+use crate::marker::Overworld;
 
 pub struct MeloncraftWorldManagerPlugin;
 
 impl Plugin for MeloncraftWorldManagerPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(Overworld(World::default()));
     }
 }
