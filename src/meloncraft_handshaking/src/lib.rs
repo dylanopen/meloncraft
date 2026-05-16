f7cab6188fb8a7afb56efccba3d258f5989be906
dylancode <opensource@dylancode.dev>
2026-05-16 20:16:31 +0100

    handshaking: register messages

--- src/meloncraft_handshaking/src/lib.rs
+++ src/meloncraft_handshaking/src/lib.rs
@@ -1,4 +1,5 @@
 mod handle_intention;
+pub mod message;
 
 use bevy::app::Plugin;
 use bevy::app::{App, Update};
@@ -8,5 +9,9 @@ pub struct MeloncraftHandshakingPlugin;
 impl Plugin for MeloncraftHandshakingPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, handle_intention::update_connection_states);
+
+        app.add_message::<message::StatusHandshakeReceived>();
+        app.add_message::<message::LoginHandshakeReceived>();
+        app.add_message::<message::TransferHandshakeReceived>();
     }
 }
