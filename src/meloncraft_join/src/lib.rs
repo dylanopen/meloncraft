aa2ac678bbe9cee62150764cff4edd37f5412159
dylancode <opensource@dylancode.dev>
2026-05-16 20:17:05 +0100

    new crate: meloncraft_join

---
+++ src/meloncraft_join/src/lib.rs
@@ -0,0 +1,10 @@
+use bevy::app::{App, Plugin};
+use meloncraft_handshaking::MeloncraftHandshakingPlugin;
+
+pub struct MeloncraftJoinPlugin;
+
+impl Plugin for MeloncraftJoinPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(MeloncraftHandshakingPlugin);
+    }
+}
