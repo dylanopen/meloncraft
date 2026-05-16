2989a2d2f12cc855d784a27a53e68d4bc8801bf2
dylancode <opensource@dylancode.dev>
2026-05-16 21:03:24 +0100

    create message ClientLocaleReceived

--- src/meloncraft_login/src/messages.rs
+++ src/meloncraft_login/src/messages.rs
@@ -1,9 +1,15 @@
 use bevy::ecs::message::Message;
 use bevy::prelude::Entity;
-use meloncraft_player::GameProfile;
+use meloncraft_player::{GameProfile, Locale};
 
 #[derive(Message, Debug, Clone)]
 pub struct OfflineLoggedIn {
     pub client: Entity,
     pub profile: GameProfile,
+}
+
+#[derive(Message, Debug, Clone)]
+pub struct ClientLocaleReceived {
+    pub client: Entity,
+    pub locale: Locale,
 }
