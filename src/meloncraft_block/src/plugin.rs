1f28b1b725b182bf80f668cbc1641c562d1ba409
dylancode <opensource@dylancode.dev>
2026-05-17 16:11:51 +0100

    create message PlayerBrokeBlock

--- src/meloncraft_block/src/plugin.rs
+++ src/meloncraft_block/src/plugin.rs
@@ -1,10 +1,11 @@
 use bevy::app::{App, Plugin};
-use crate::broken::BlockBroken;
+use crate::broken::{BlockBroken, PlayerBrokeBlock};
 
 pub struct MeloncraftBlockPlugin;
 
 impl Plugin for MeloncraftBlockPlugin {
     fn build(&self, app: &mut App) {
         app.add_message::<BlockBroken>();
+        app.add_message::<PlayerBrokeBlock>();
     }
 }
