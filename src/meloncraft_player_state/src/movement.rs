66643b2524bbb58e1a73cab33ca52f10a67d98f2
dylancode <opensource@dylancode.dev>
2026-05-17 10:01:22 +0100

    create player_state handler movement::fwd_player_moved

--- src/meloncraft_player_state/src/movement.rs
+++ src/meloncraft_player_state/src/movement.rs
@@ -1,13 +1,27 @@
 use bevy::ecs::system::Commands;
-use bevy::prelude::MessageReader;
+use bevy::prelude::{MessageReader, MessageWriter};
+use meloncraft_entity::player::moved::PlayerMoved;
 use meloncraft_packets::ServerboundSetPlayerPosition;
 
 pub fn save_new_location(
-    mut set_position_pr: MessageReader<ServerboundSetPlayerPosition>,
+    mut player_moved_mr: MessageReader<PlayerMoved>,
     mut commands: Commands,
 ) {
+    for player_moved in player_moved_mr.read() {
+        commands.entity(player_moved.entity)
+            .insert(player_moved.new_position.clone());
+    }
+}
+
+pub fn fwd_player_moved(
+    mut set_position_pr: MessageReader<ServerboundSetPlayerPosition>,
+    mut player_moved_mw: MessageWriter<PlayerMoved>,
+) {
     for packet in set_position_pr.read() {
-        commands.entity(packet.client)
-            .insert(packet.position.clone());
+        player_moved_mw.write(PlayerMoved {
+            entity: packet.client,
+            old_position: packet.position.clone(), // For now, just set the old position to the new position, but this will cause future bugs so will soon be replaced.
+            new_position: packet.position.clone(),
+        });
     }
 }
