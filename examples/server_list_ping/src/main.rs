a80f01d921e0f1e7ab90dbf6a390c0d1a8f0a6cd
dylancode <opensource@dylancode.dev>
2026-05-17 22:58:07 +0100

    update imports in example server_list_ping

--- examples/server_list_ping/src/main.rs
+++ examples/server_list_ping/src/main.rs
@@ -5,12 +5,12 @@ use bevy::prelude::{MessageReader, MessageWriter};
 use meloncraft::handshaking::MeloncraftHandshakingPlugin;
 use meloncraft::network::MeloncraftNetworkPlugin;
 use meloncraft::packets::MeloncraftPacketsPlugin;
-use meloncraft::packets::clientbound::login::ClientboundLoginDisconnect;
-use meloncraft::packets::serverbound::login::ServerboundLoginStart;
+use meloncraft::packets::ClientboundLoginDisconnect;
+use meloncraft::packets::ServerboundLoginStart;
 use meloncraft::server_list::MeloncraftServerListPlugin;
-use meloncraft_server_info::max_players::MaxPlayers;
-use meloncraft_server_info::motd::Motd;
-use meloncraft_server_info::online_players::OnlinePlayers;
+use meloncraft::server_info::max_players::MaxPlayers;
+use meloncraft::server_info::motd::Motd;
+use meloncraft::server_info::online_players::OnlinePlayers;
 use meloncraft::text::JsonText;
 use std::time::Duration;
 
