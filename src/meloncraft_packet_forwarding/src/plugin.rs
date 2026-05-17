0c7a4180683a8392bfda8b4655fce05249dba549
dylancode <opensource@dylancode.dev>
2026-05-17 15:44:53 +0100

    create PacketForwardingPlugin

---
+++ src/meloncraft_packet_forwarding/src/plugin.rs
@@ -0,0 +1,9 @@
+use bevy::app::{App, Plugin, Update};
+
+pub struct MeloncraftPacketForwardingPlugin;
+
+impl Plugin for MeloncraftPacketForwardingPlugin {
+    fn build(&self, app: &mut App) {
+
+    }
+}
