28352fea4f362ee68daac43d923ebf544b728e22
dylancode <opensource@dylancode.dev>
2026-05-17 10:41:50 +0100

    impl from/to u8 for PlayerActionStatus

--- src/meloncraft_player/src/action_status.rs
+++ src/meloncraft_player/src/action_status.rs
@@ -8,3 +8,34 @@ pub enum PlayerActionStatus {
     UpdateHeldItem,
     SwapOffhand,
 }
+
+impl TryFrom<u8> for PlayerActionStatus {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        return match value {
+            0 => Ok(Self::StartedDigging),
+            1 => Ok(Self::CancelledDigging),
+            2 => Ok(Self::FinishedDigging),
+            3 => Ok(Self::DropItemStack),
+            4 => Ok(Self::DropSingleItem),
+            5 => Ok(Self::UpdateHeldItem),
+            6 => Ok(Self::SwapOffhand),
+            _ => Err(()),
+        }
+    }
+}
+
+impl From<PlayerActionStatus> for u8 {
+    fn from(value: PlayerActionStatus) -> Self {
+        return match value {
+            PlayerActionStatus::StartedDigging => 0,
+            PlayerActionStatus::CancelledDigging => 1,
+            PlayerActionStatus::FinishedDigging => 2,
+            PlayerActionStatus::DropItemStack => 3,
+            PlayerActionStatus::DropSingleItem => 4,
+            PlayerActionStatus::UpdateHeldItem => 5,
+            PlayerActionStatus::SwapOffhand => 6,
+        }
+    }
+}
