6c239002bf57f766ef5d75d05791d4c1eee95bc7
dylancode <opensource@dylancode.dev>
2026-05-18 16:38:52 +0100

    document IntentionType

--- src/meloncraft_client/src/intention_type.rs
+++ src/meloncraft_client/src/intention_type.rs
@@ -1,9 +1,29 @@
 use core::fmt;
 
+/// The intention type of a handshake packet.
+/// Indicates whether the client is trying to:
+/// - Query the server's status, done when pinging the server in the server list -
+///   [`IntentionType::Status`].
+/// - Log in to the server, which happens when they click on our server from the server list -
+///   [`IntentionType::Login`].
+/// - Join our server because they have been sent here *from* another server, which happens when
+///   they are transferred to our server from another server - [`IntentionType::Transfer`] (*we may
+///   want to handle this differently if we don't accept transfers*).
+///
+/// ## Connection state
+/// - The [`IntentionType`] is slightly different from the `ConnectionState` of a client.
+/// - The `ConnectionState` is the client's current connection state, which is used to determine how
+///   to parse any serverbound packets from the client.
+/// - The [`IntentionType`] is the client's intention when they send the handshake packet, which
+///   *may be* used to determine the *next* `ConnectionState` of the client, but is not usually
+///   the same as the client's **current** `ConnectionState`.
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum IntentionType {
+
     Status,
+
     Login,
+
     Transfer,
 }
 
