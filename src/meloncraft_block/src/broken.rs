3854789e3236d538e0fac290245075f27e4b8f5e
dylancode <opensource@dylancode.dev>
2026-05-19 11:45:28 +0100

    document BlockBroken

--- src/meloncraft_block/src/broken.rs
+++ src/meloncraft_block/src/broken.rs
@@ -4,6 +4,15 @@ use bevy::ecs::message::Message;
 use bevy::math::IVec3;
 use bevy::prelude::Entity;
 
+/// Message sent whenever a block is broken by any cause.
+///
+/// You should listen to this event if you want to do something when *any* block is broken,
+/// regardless of the cause.
+/// 
+/// ## Alternatives
+/// - If you want to respond only when a player breaks a block, and ignore other causes, listen to
+///   the [`PlayerBrokeBlock`] message instead, which wraps this message as a submessage, as well as
+///   other info.
 #[derive(Message, Debug, Clone)]
 pub struct BlockBroken {
     pub block_location: IVec3,
