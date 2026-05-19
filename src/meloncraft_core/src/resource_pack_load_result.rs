fad9ef08bea21a4e65a89121b5ad6a6a20ec065b
dylancode <opensource@dylancode.dev>
2026-05-19 11:10:55 +0100

    document ResourcePackLoadResult variants

--- src/meloncraft_core/src/resource_pack_load_result.rs
+++ src/meloncraft_core/src/resource_pack_load_result.rs
@@ -8,24 +8,53 @@
 /// The variants for [`ResourcePackLoadResult`] can be represented as integers in packets. Their IDs
 /// are specified in the individual variant documentation, and `From` and `TryFrom` are implemented
 /// to convert between the enum and its integer representation.
+///
+/// See <https://minecraft.wiki/w/Java_Edition_protocol/Packets#Resource_Pack_Response_(configuration)>.
 #[derive(Debug, Clone)]
 pub enum ResourcePackLoadResult {
 
-    Success = 0,
+    /// The client successfully loaded the resource pack.
+    /// This is sent by the client after successfully downloading *and loading* the resource
+    /// pack requested. It may not be the first result received.
+    /// **ID `0` in packets.**
+    Success,
 
-    Declined = 1,
+    /// The client declined to load the resource pack. This is because they chose not to load the
+    /// pack when prompted.
+    /// **ID `1` in packets.**
+    Declined,
 
-    Failed = 2,
+    /// The client failed to load the resource pack. This is probably because an error occurred while
+    /// downloading or loading the pack, such as a network error.
+    /// **ID `2` in packets.**
+    Failed,
 
-    Accepted = 3,
+    /// The client accepted to load the resource pack, but has not finished downloading it yet. This
+    /// is probably sent by the client immediately after accepting to load the resource pack, and is
+    /// followed by either the `Success` variant if the client finishes downloading and loading the
+    /// pack, or the `Failed` variant if the client fails to download or load the pack.
+    /// **ID `3` in packets.**
+    Accepted,
 
-    Downloaded = 4,
+    /// The client successfully downloaded the resource pack, but has not finished loading it yet.
+    /// This should be sent after the download has finished, but their client hasn't finished
+    /// actually deserializing and loading the pack yet. It should be followed by either the
+    /// `Success` variant if the client finishes loading the pack, or the `Failed` variant if they
+    /// fails to load the pack.
+    Downloaded,
 
-    InvalidUrl = 5,
+    /// The client couldn't download the resource pack because the URL provided by the server was
+    /// invalid. This is probably sent close to immediately after the
+    /// [`ResourcePackLoadResult::Accepted`] variant, since the client would only try to download
+    /// the pack after accepting to load it.
+    /// **ID `5` in packets.**
+    InvalidUrl,
 
-    ReloadFailed = 6,
+    /// **ID `6` in packets.**
+    ReloadFailed,
 
-    Discarded = 7,
+    /// **ID `7` in packets.**
+    Discarded,
 }
 
 impl TryFrom<i32> for ResourcePackLoadResult {
