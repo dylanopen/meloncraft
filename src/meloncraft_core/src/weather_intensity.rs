2052ad35b90f36742edb6a4396d0da8d1a4a8f3d
dylancode <opensource@dylancode.dev>
2026-05-19 10:52:39 +0100

    document WeatherIntensity

--- src/meloncraft_core/src/weather_intensity.rs
+++ src/meloncraft_core/src/weather_intensity.rs
@@ -1,5 +1,16 @@
-/// The intensity of the rain or thunder, as a value between 0.0 and 1.0.
-/// 0.0 means no rain/thunder, 1.0 means full intensity.
+/// Represents the intensity of rain or thunder in the game world.
+/// 
+/// ## Fields
+/// 0. The intensity of the rain or thunder, as an `f32` value between 0.0 and 1.0.
+///    0.0 means no rain/thunder, 1.0 means full intensity.
+///
+/// ## Packet usage
+/// Used in some variants of the `ClientboundGameEvent` packet to represent the intensity of rain or
+/// thunder in the game world. See that packet's documentation for more information.
+///
+/// ## Constraints
+/// - The value of `WeatherIntensity` must be between `0.0` and `1.0`, inclusive. Values outside
+///   this range may cause packets to fail to send or be ignored by the client.
 #[derive(Debug, Clone)]
 pub struct WeatherIntensity(pub f32);
 
