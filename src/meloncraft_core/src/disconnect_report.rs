6ef136106fc3d67e685dc7bbf2fd555898fc63bb
dylancode <opensource@dylancode.dev>
2026-05-19 09:39:46 +0100

    document DisconnectReport

--- src/meloncraft_core/src/disconnect_report.rs
+++ src/meloncraft_core/src/disconnect_report.rs
@@ -1,5 +1,18 @@
+//! Module for [`DisconnectReport`] struct.
+
+/// Stores a single disconnect report.
+/// These are shown to the client if they get disconnected or, sometimes, if their game crashes.
+///
+/// As a server owner, you can create multiple [`DisconnectReport`]s and send them in the packet
+/// listed below, to change the disconnect message a client receives.
+///
+/// ## Packets
+/// Used in the `ClientboundCustomReportDetails` packet during *configuration*.
+/// See that packet's documentation for more information.
 #[derive(Debug, Clone)]
 pub struct DisconnectReport {
+
     pub title: String,
+
     pub description: String,
 }
