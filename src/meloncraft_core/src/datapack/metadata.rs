8a55b5ad05d7152a5cecdf8b9d16843adf68c212
dylancode <opensource@dylancode.dev>
2026-05-19 10:55:48 +0100

    document DatapackMetadata

--- src/meloncraft_core/src/datapack/metadata.rs
+++ src/meloncraft_core/src/datapack/metadata.rs
@@ -1,3 +1,13 @@
+//! Module for struct [`DatapackMetadata`].
+
+/// Represents the metadata of a datapack, which is used to identify a datapack by its information
+/// in the game.
+///
+/// See <https://minecraft.wiki/w/Data_pack> for general information about datapacks.
+///
+/// ## Packet usage
+/// Potentially used in many places: the main ones that come to mind is
+/// `ClientboundSelectKnownPacks` and `ServerboundSelectKnownPacks`.
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct DatapackMetadata {
     pub namespace: String,
