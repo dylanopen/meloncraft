edfd930643efb9c2c662c6aa816fba630c67974e
dylancode <opensource@dylancode.dev>
2026-05-19 22:17:38 +0100

    document ChunkBlockSection

--- src/meloncraft_chunk/src/block_section.rs
+++ src/meloncraft_chunk/src/block_section.rs
@@ -1,14 +1,36 @@
+//! Module for [`ChunkBlockSection`] struct.
+
 use crate::biome::Biome;
 use meloncraft_block::block::Block;
 
+/// Represents a single, immutable, 16x16x16 cube of blocks and biomes.
+///
+/// This struct should be a 'final product'. You probably shouldn't store it anywhere, instead, you
+/// should use another struct for storage and eventually convert to chunksections when you need to
+/// talk to the client.
+///
+/// ## Packet usage
+/// [`ChunkBlockSection`]s are the main way chunk data is sent to clients over the network. Most
+/// chunk wrapping structs will provide methods for converting to \[a list of\]
+/// [`ChunkBlockSection`]\[s\].
+///
+/// ## Mutation
+/// - You **should not manually mutate** the fields in this struct. They are intended to be
+///   read-only.
+/// - Mutation will mean that the `block_count` becomes out of date unless you manually update it
+///   when changing a block.
 #[derive(Debug, Clone)]
 pub struct ChunkBlockSection {
+
     pub block_count: i16,
+
     pub blocks: [Block; 4096],
+
     pub biomes: [Biome; 64],
 }
 
 impl ChunkBlockSection {
+
     #[must_use]
     pub fn new(blocks: [Block; 4096], biomes: [Biome; 64]) -> Self {
         let block_count = sum_non_air_blocks(&blocks);
