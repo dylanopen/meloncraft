cff2d1df973df3973286e8e2f1f4a0c4cdcd4d2f
dylancode <opensource@dylancode.dev>
2026-05-19 09:47:04 +0100

    document DisconnectReport::description

--- src/meloncraft_core/src/disconnect_report.rs
+++ src/meloncraft_core/src/disconnect_report.rs
@@ -4,7 +4,7 @@
 /// These are shown to the client if they get disconnected or, sometimes, if their game crashes.
 ///
 /// As a server owner, you can create multiple [`DisconnectReport`]s and send them in the packet
-/// listed below, to change the disconnect message a client receives.
+/// listed below, to change the disconnect message a client receives (saved in the client logs).
 ///
 /// ## Packets
 /// Used in the `ClientboundCustomReportDetails` packet during *configuration*.
@@ -17,9 +17,16 @@ pub struct DisconnectReport {
     /// [`DisconnectReport::description`] field.
     ///
     /// ## Constraints
-    /// - The [`DisconnectReport::title`] must be **less than or equal to** `128` characters in
+    /// - The [`DisconnectReport::title`] must be **less than or equal to** `128` ASCII characters in
     ///   length, or packets may fail to send.
     pub title: String,
 
+    /// The extra details in the disconnect report shown to the client.
+    /// This is for a full explanation of the disconnect. Please write a summary of the issue in the
+    /// [`DisconnectReport::title`] field.
+    ///
+    /// ## Constraints
+    /// - The [`DisconnectReport::description`] must be **less than or equal to** `4096` ASCII
+    ///   characters in length, or packets may fail to send.
     pub description: String,
 }
