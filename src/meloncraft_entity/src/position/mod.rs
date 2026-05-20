9894d5f849cee4a4ba0e9d49839488a9d4020418
dylancode <opensource@dylancode.dev>
2026-05-20 08:19:50 +0100

    document EntityPosition

--- src/meloncraft_entity/src/position/mod.rs
+++ src/meloncraft_entity/src/position/mod.rs
@@ -1,3 +1,5 @@
+//! Module for struct [`EntityPosition`] and submodules related to it.
+
 use bevy::ecs::component::Component;
 use bevy::math::DVec3;
 
@@ -6,8 +8,18 @@ pub mod last;
 
 use crate::position::flags::EntityPositionFlags;
 
+/// Component for storing an entity's position in the world, as well as some extra metadata about
+/// the position (see [`EntityPositionFlags`]).
+///
+/// You can add this component to any players or other entities that have a position, in order to
+/// store their position as a component.
+///
+/// See the documentation for the individual fields for more information about the data stored in
+/// this component.
 #[derive(Component, Debug, Clone)]
 pub struct EntityPosition {
+
     pub location: DVec3,
+
     pub flags: EntityPositionFlags,
 }
