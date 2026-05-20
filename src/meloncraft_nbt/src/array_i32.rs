28d07a215ddfa4970bde1ab71113b7326d50166d
dylancode <opensource@dylancode.dev>
2026-05-20 16:10:21 +0100

    document NbtArrayI32

--- src/meloncraft_nbt/src/array_i32.rs
+++ src/meloncraft_nbt/src/array_i32.rs
@@ -1,7 +1,26 @@
+//! Module for struct [`NbtArrayI32`].
+
 use core::ops::{Deref, DerefMut};
 
+/// NBT wrapper to represent **a list of** 32-bit signed integers (a `Vec` of `i32`).
+///
+/// These are a special type of list in the NBT format; this struct acts as a marker that the
+/// contained value should be treated as an NBT array of i32s.
+/// 
+/// ## Deref
+/// Dereferencing this (and most other [`NbtValue`](`crate::NbtValue`)s) will yield the wrapped type.
+/// In this case, a `Vec<i32>` representing all the bytes in the array.
+///
+/// ## Serialization
+/// To serialize or deserialize an [`NbtArrayI32`] to/from a network protocol format, you should use the
+/// traits provided by the `meloncraft_protocol_types` crate, which implements `ProtocolType` on the
+/// [`NbtValue`](`crate::NbtValue`) and [`NbtTag`](`crate::NbtTag`) types.
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct NbtArrayI32(pub Vec<i32>);
+pub struct NbtArrayI32(
+
+    /// A `Vec` containing a variable number of `i32`s. This is the *value* of the NBT value / tag.
+    pub Vec<i32>
+);
 
 impl Deref for NbtArrayI32 {
     type Target = Vec<i32>;
