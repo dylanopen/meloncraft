6e4edd7606199f57eba38d6a2d9550b3f77ccedc
dylancode <opensource@dylancode.dev>
2026-05-20 16:39:47 +0100

    document NbtList

--- src/meloncraft_nbt/src/list.rs
+++ src/meloncraft_nbt/src/list.rs
@@ -1,8 +1,44 @@
+//! Module for struct [`NbtList`].
+
 use crate::NbtValue;
 use core::ops::{Deref, DerefMut};
 
+/// NBT wrapper to represent **a list of a single data type**.
+/// Please See [`NbtList::0`] for more information about what an [`NbtList`] stores.
+/// 
+/// ## Deref
+/// Dereferencing this (and most other [`NbtValue`](`crate::NbtValue`)s) will yield the wrapped type.
+/// In this case, a `Vec<NbtValue>` representing all the [`NbtValue`]s in the array.
+///
+/// ## Static typing
+/// **`NbtList`s are not fully dynamic!**
+/// All elements should be of the same type, or this struct will not serialize properly.
+///
+/// ## Serialization
+/// To serialize or deserialize an [`NbtList`] to/from a network protocol format, you should use the
+/// traits provided by the `meloncraft_protocol_types` crate, which implements `ProtocolType` on the
+/// [`NbtValue`](`crate::NbtValue`) and [`NbtTag`](`crate::NbtTag`) types.
 #[derive(Debug, Clone, PartialEq)]
-pub struct NbtList(pub Vec<NbtValue>);
+pub struct NbtList(
+
+    /// A `Vec` containing the [`NbtValue`]s contained within this list.
+    ///
+    /// It seems like lists can contain other lists and compounds (and maybe arrays), but I can't
+    /// confirm this at the time of writing this documentation.
+    ///
+    /// ## Constraints
+    /// - All values in this vec should be of the same NBT type! Serializers should catch this and
+    ///   panic, but please also be sure yourself.
+    ///
+    /// ## Alternatives
+    /// If possible, you should use an array. The three NBT arrays are:
+    /// - [`NbtArrayU8`](`crate::NbtArrayU8`) for a `Vec<u8>`
+    /// - [`NbtArrayI32`](`crate::NbtArrayI32`) for a `Vec<i32>`
+    /// - [`NbtArrayI64`](`crate::NbtArrayI64`) for a `Vec<i64>`
+    ///
+    /// Check the protocol to find out if your specific use case uses a list or array.
+    pub Vec<NbtValue>,
+);
 
 impl Deref for NbtList {
     type Target = Vec<NbtValue>;
