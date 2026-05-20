e0316ca8575ff458b4e9ffb02b3f4a0158fbd070
dylancode <opensource@dylancode.dev>
2026-05-20 08:53:03 +0100

    document MeloncraftEntityPlugin

--- src/meloncraft_entity/src/plugin.rs
+++ src/meloncraft_entity/src/plugin.rs
@@ -1,6 +1,14 @@
+//! Module for [`MeloncraftEntityPlugin`].
+
 use bevy::app::{App, Plugin};
 use crate::player::plugin::MeloncraftPlayerEntityPlugin;
 
+/// Meloncraft plugin to initialize messages related to entities, as well as adding sub-plugins for
+/// specific types of entities.
+///
+/// ## Sub-plugins
+/// Adding this plugin will register the following other plugins:
+/// - [`MeloncraftPlayerEntityPlugin`]
 pub struct MeloncraftEntityPlugin;
 
 impl Plugin for MeloncraftEntityPlugin {
