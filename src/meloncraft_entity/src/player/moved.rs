7407eb47120a0fe506a87b6f020ba8708fa1067c
dylancode <opensource@dylancode.dev>
2026-05-20 08:41:48 +0100

    document PlayerMoved

--- src/meloncraft_entity/src/player/moved.rs
+++ src/meloncraft_entity/src/player/moved.rs
@@ -1,10 +1,30 @@
+//! Module for message struct [`PlayerMoved`].
+
 use bevy::ecs::message::Message;
 use bevy::prelude::Entity;
 use crate::position::EntityPosition;
 
+/// Message sent when a player moves to a new position.
+///
+/// Stores the entity, their new position and their old position.
+/// See the individual struct fields for more information about each stored field.
+///
+/// ## Writing
+/// You should write (send) this message when a player moves from one position to another, such as
+/// when they move in the world or teleport.
+///
+/// ## Reading
+/// You can read (receive) this message to be notified when a player moves to a new position, and to
+/// get information about the player's old and new positions.
+/// For example, you might want to write a system which listens for this message and broadcasts the
+/// player's new position to all other players in the world, or to update the player's position
+/// component.
 #[derive(Debug, Clone, Message)]
 pub struct PlayerMoved {
+
     pub entity: Entity,
+
     pub old_position: EntityPosition,
+
     pub new_position: EntityPosition,
 }
