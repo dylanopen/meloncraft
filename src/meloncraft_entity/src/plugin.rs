a01ccdd162d50cc88af41e2ddef22a649adddbff
dylancode <opensource@dylancode.dev>
2026-05-20 08:49:35 +0100

    document MeloncraftPlayerEntityPlugin

--- src/meloncraft_entity/src/plugin.rs
+++ src/meloncraft_entity/src/plugin.rs
@@ -1,10 +1,10 @@
 use bevy::app::{App, Plugin};
-use crate::player::plugin::MeloncraftPlayerPositionPlugin;
+use crate::player::plugin::MeloncraftPlayerEntityPlugin;
 
 pub struct MeloncraftEntityPlugin;
 
 impl Plugin for MeloncraftEntityPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(MeloncraftPlayerPositionPlugin);
+        app.add_plugins(MeloncraftPlayerEntityPlugin);
     }
 }
