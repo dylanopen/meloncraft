b4c11986a59e79f671d32af223aec4be45a769b1
dylancode <opensource@dylancode.dev>
2026-05-21 21:09:52 +0100

    document NbtTag

--- src/meloncraft_nbt/src/tag.rs
+++ src/meloncraft_nbt/src/tag.rs
@@ -1,8 +1,19 @@
+//! Module for struct [`NbtTag`].
+
 use crate::value::NbtValue;
 
+/// Stores a key-value pair for an NBT tag (in an [`NbtCompound`](`crate::NbtCompound`)).
+/// This can act similarly to a hashmap entry.
+///
+/// ## Alternatives
+/// - If you just need the value, not the key, use [`NbtValue`].
+/// - If in addition, you know the NBT tag tyep of your data, you can use a specific NBT type from
+///   the [`meloncraft_nbt`](`crate`) crate (e.g. `NbtU8`).
 #[derive(Debug, Clone, PartialEq)]
 pub struct NbtTag {
+
     pub key: String,
+
     pub value: NbtValue,
 }
 
