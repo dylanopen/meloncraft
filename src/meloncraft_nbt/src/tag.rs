879e734ae258a443f2b0520ec760dadbe94faecf
dylancode <opensource@dylancode.dev>
2026-05-21 21:19:00 +0100

    document NbtTag::key

--- src/meloncraft_nbt/src/tag.rs
+++ src/meloncraft_nbt/src/tag.rs
@@ -5,6 +5,9 @@ use crate::value::NbtValue;
 /// Stores a key-value pair for an NBT tag (in an [`NbtCompound`](`crate::NbtCompound`)).
 /// This can act similarly to a hashmap entry.
 ///
+/// It is essentially a compounded type of both a key (`String`) and a value ([`NbtValue`]).
+/// You are advised to use this struct if you need to store both the NBT's key *and* value.
+///
 /// ## Alternatives
 /// - If you just need the value, not the key, use [`NbtValue`].
 /// - If in addition, you know the NBT tag tyep of your data, you can use a specific NBT type from
@@ -12,6 +15,15 @@ use crate::value::NbtValue;
 #[derive(Debug, Clone, PartialEq)]
 pub struct NbtTag {
 
+    /// The key that can be used to refer to this tag.
+    ///
+    /// If this tag is part of a compound, this can be used as an index to this tag's [`NbtValue`],
+    /// see the [`NbtTag::value`] field.
+    ///
+    /// ## Root compound
+    /// To represent the *root compound* (the outermost compound used in many different structures
+    /// of NBT data) you can just use an empty string (`""`) as the key, since the root compound does not have a key.
+    /// Some other implementations may use a different string as the key for the root compound.
     pub key: String,
 
     pub value: NbtValue,
