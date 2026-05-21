b5917dfeb3f2359c835d4701bc7890b14091bb0c
dylancode <opensource@dylancode.dev>
2026-05-21 22:44:08 +0100

    document AllowPlayerListings

--- src/meloncraft_player/src/allow_player_listings.rs
+++ src/meloncraft_player/src/allow_player_listings.rs
@@ -1,4 +1,15 @@
+//! Module for component struct [`AllowPlayerListings`].
+
 use bevy::prelude::Component;
 
+/// Component storing whether the player has consented to being visible in player listings.
+/// *Component for a player/client entity*.
+///
+/// - If true, the player **will** be shown in listings, e.g. when hoving over the player count
+///   in the multiplayer menu, their name will be shown.
+/// - If false, the server **should not** display their username in listings. You may want to show
+///   'anonymous player' or something similar.
+///
+/// You technically do not have to acknowledge this request, but you *should*.
 #[derive(Component, Clone, Debug, Copy, Eq, PartialEq)]
 pub struct AllowPlayerListings(pub bool);
