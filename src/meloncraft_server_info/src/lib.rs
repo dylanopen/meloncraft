614cf2b34ebbf9e9cb1e32922d65560dbfcac3e8
dylancode <opensource@dylancode.dev>
2026-05-22 16:29:30 +0100

    document crate server_info

--- src/meloncraft_server_info/src/lib.rs
+++ src/meloncraft_server_info/src/lib.rs
@@ -1,3 +1,21 @@
+//! Crate containing data types and resources for the current state of the Meloncraft server, as
+//! well as various configuration options (e.g. server MOTD, max players, etc.) as well as any other
+//! data type related to the server's info.
+//! For the [Meloncraft](https://github.com/dylanopen/meloncraft) server implementation.
+//!
+//! See the documentation for the individual modules, structs and enums for more information on the
+//! resources contained in this crate, and their usage in packets and systems throughout the
+//! codebase.
+//!
+//! ## Pure `type` crate
+//! `meloncraft_server_info` is a *type* crate. This means it doesn't hold any systems or stateful
+//! functions. It just holds datatypes and resources related to the server's current / configured
+//! state, types which themselves are *used* by systems and functions in other crates.
+//! Impure functions are not allowed in this crate, and so should any functions not directly related
+//! to operating on the datatype itself only (utility functions for conversions, instantiation,
+//! logic, etc. are fine but only if they operate on the datatype itself, and not on any external
+//! state or external mutable parameters).
+
 pub mod max_players;
 pub mod motd;
 pub mod online_players;
