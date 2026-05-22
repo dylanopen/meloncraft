977551056cec134340d8c6237076c0c1eed394f3
dylancode <opensource@dylancode.dev>
2026-05-22 08:27:17 +0100

    document ChatMode::Hidden

--- src/meloncraft_player/src/chat_mode.rs
+++ src/meloncraft_player/src/chat_mode.rs
@@ -21,6 +21,8 @@ pub enum ChatMode {
     /// *all* types of chat messages, including normal chat messages, system messages, and command
     /// feedback.
     ///
+    /// **Protocol ID: `0`**.
+    ///
     /// ## Enabled clientbound packets
     /// The server should send these types of packets to a player with this chat mode:
     /// - `ClientboundPlayerChatMessage`: Player-initiated chat messages, including the commands
@@ -29,7 +31,7 @@ pub enum ChatMode {
     ///   /me, /msg, /tell, /w and /teammsg.
     /// - `ClientboundSystemChatMessage`: Feedback from running a command, such as "Your game mode
     ///   has been updated to creative." 
-    /// - `CilentboundSystemChatMessageOverlay`: Game state information that is displayed above
+    /// - `CilentboundSystemChatMessage` *with overlay*: Game state information that is displayed above
     ///   the hot bar, such as "You may not rest now, the bed is too far away". 
     ///
     /// ## Disabled clientbound packets
@@ -51,11 +53,13 @@ pub enum ChatMode {
     /// [`ChatMode`] component variant on a player, indicating that the player would like to receive
     /// command feedback and system messages, but *not* normal chat messages **or** whispers.
     ///
+    /// **Protocol ID: `1`**.
+    ///
     /// ## Enabled clientbound packets
     /// The server should send only these types of packets to a player with this chat mode:
     /// - `ClientboundSystemChatMessage`: Feedback from running a command, such as "Your game mode
     ///   has been updated to creative." 
-    /// - `CilentboundSystemChatMessageOverlay`: Game state information that is displayed above
+    /// - `CilentboundSystemChatMessage` *with overlay*: Game state information that is displayed above
     ///   the hot bar, such as "You may not rest now, the bed is too far away". 
     ///
     /// ## Disabled clientbound packets
@@ -77,6 +81,39 @@ pub enum ChatMode {
     /// - `ServerboundChatMessage`: When the player sends a normal chat message, such as "Hello
     ///   world!".
     CommandsOnly,
+
+    /// [`ChatMode`] component variant on a player, indicating that the player would not like to
+    /// receive *any* chat messages at all, including normal chat messages, system messages, and
+    /// command feedback.
+    /// They would only like to receive game state information that is displayed above the hot bar,
+    /// such as "You may not rest now, the bed is too far away" (an 'essential' message).
+    ///
+    /// **Protocol ID: `2`**.
+    ///
+    /// ## Enabled clientbound packets
+    /// The server should send only these types of packets to a player with this chat mode:
+    /// - `CilentboundSystemChatMessage` *with overlay*: Game state information that is displayed above
+    ///   the hot bar, such as "You may not rest now, the bed is too far away". 
+    ///
+    /// ## Disabled clientbound packets
+    /// The server **should not** send these types of packets to a player with this chat mode:
+    /// - `ClientboundPlayerChatMessage`: Player-initiated chat messages, including the commands
+    ///   /say, /me, /msg, /tell, /w and /teammsg.
+    /// - `ClientboundDisguisedChatMessage`: Messages sent by non-players using the commands /say,
+    ///   /me, /msg, /tell, /w and /teammsg.
+    /// - `ClientboundSystemChatMessage`: Feedback from running a command, such as "Your game mode
+    ///   has been updated to creative." 
+    ///
+    /// ## Enabled serverbound packets
+    /// Clients in the [`ChatMode::Hidden`] state *shouldn't send any chat-related packets*.
+    ///
+    /// ## Disabled serverbound packets
+    /// The *client* **will not** send these types of packets to the server when the player has
+    /// this chat mode:
+    /// - `ServerboundChatMessage`: When the player sends a normal chat message, such as "Hello
+    ///   world!".
+    /// - `ServerboundChatCommand`: When the player runs any Minecraft command, such as "/gamemode
+    ///   creative" or "/msg meloncrafter Hello world!".
     Hidden,
 }
 
