157955bde2428a29b5168019f8f6d56976ee9e7f
dylancode <opensource@dylancode.dev>
2026-05-22 10:39:14 +0100

    document GameProfile::uuid

--- src/meloncraft_player/src/game_profile.rs
+++ src/meloncraft_player/src/game_profile.rs
@@ -11,6 +11,21 @@ use bevy::prelude::Component;
 /// entity as soon as you identify their username and UUID.
 #[derive(Component, Debug, Clone)]
 pub struct GameProfile {
+
+    /// The *universally unique identifier* ([`Uuid`]) defining the player.
+    /// Every player (and in fact entity) will have a UUID.
+    ///
+    /// UUIDs are represented by a `u128`. See [`Uuid`] for more information.
+    ///
+    /// # Uniqueness
+    /// This is basically guaranteed to be a completely random identifier, never seen before. This
+    /// means it is safe (and recommended) to use UUIDs to represent a player in, for example,
+    /// playerdata storage of a world.
+    ///
+    /// ## Static
+    /// UUIDs are static: a player can't change it like they can their username.
+    /// This means it is recommended to identify players by their [`GameProfile::uuid`] rather than
+    /// by their [`GameProfile::username`].
     pub uuid: Uuid,
     pub username: String,
 }
