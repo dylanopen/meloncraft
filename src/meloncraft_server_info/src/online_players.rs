7ad6acfd73496d8c11a4beb77b36b854de497137
dylancode <opensource@dylancode.dev>
2026-05-22 16:25:46 +0100

    document OnlinePlayers

--- src/meloncraft_server_info/src/online_players.rs
+++ src/meloncraft_server_info/src/online_players.rs
@@ -1,6 +1,23 @@
+//! Module for struct resource [`OnlinePlayers`].
+
 use bevy::ecs::resource::Resource;
 use core::ops::{Deref, DerefMut};
 
+/// The server's *current count* of the *number of players* that are online on the server at the
+/// moment.
+///
+/// You may also want to reference this resource whenever a client sends a status request
+/// (server list ping) , as the Minecraft server list shows the number of players currently online.
+/// well as the maximum number of players that can join the server.
+///
+/// This is a global resource; you can add it with Bevy to change the number of displayed online
+/// players.
+/// You should probably insert this resource initially with a count of zero, then access and
+/// increment it whenever a player joins/leaves the game.
+///
+/// ## Future breaking changes
+/// In the future, this struct will likely also store the *list of player entities* that are online
+/// on the server, instead of the number of online players.
 #[derive(Resource)]
 pub struct OnlinePlayers(pub u32);
 
