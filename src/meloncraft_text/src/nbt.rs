c1713f1cc9aa6b42df27a5aeee386514606e4e57
dylancode <opensource@dylancode.dev>
2026-05-22 17:51:40 +0100

    document NbtText

--- src/meloncraft_text/src/nbt.rs
+++ src/meloncraft_text/src/nbt.rs
@@ -1,7 +1,15 @@
+//! Module for enum [`NbtText`].
+
 use meloncraft_nbt::{NbtCompound, NbtString};
 
+/// Binary, NBT format for representing text.
+///
+/// Text in Meloncraft can be represented as an [`NbtValue`](meloncraft_nbt::NbtValue), using this
+/// struct. This is the main type for text, and it can be either a plain string or
+/// formatted text, depending on the variant you create.
 #[derive(Debug, Clone)]
 pub enum NbtText {
     Plain(NbtString),
     Formatted(NbtCompound),
 }
+
