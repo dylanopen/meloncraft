615b8f27562e837bd3799b07a0d98ae2771d9657
dylancode <opensource@dylancode.dev>
2026-05-22 12:32:40 +0100

    document MainHand

--- src/meloncraft_player/src/main_hand.rs
+++ src/meloncraft_player/src/main_hand.rs
@@ -1,5 +1,18 @@
+//! Module for enum component [`MainHand`].
+
 use bevy::prelude::Component;
 
+/// A player component storing the player's preference as to whether they use their left or right
+/// hand as the main hand.
+///
+/// Clients can choose which arm they want to use primarily, either left, or right (default).
+///
+/// This is likely used when forwarding a swing arm packet to all other players, and possibly
+/// others.
+///
+/// See the individual variants for the protocol IDs of each hand:
+/// - [`MainHand::Left`]
+/// - [`MainHand::Right`]
 #[derive(Component, Debug, Clone, Copy, Eq, PartialEq)]
 #[repr(i32)]
 pub enum MainHand {
