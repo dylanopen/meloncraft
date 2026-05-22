6b10edc98154d22fb204a33d115b2c3b733c3091
dylancode <opensource@dylancode.dev>
2026-05-22 12:49:26 +0100

    document EnableTextFiltering

--- src/meloncraft_player/src/text_filtering.rs
+++ src/meloncraft_player/src/text_filtering.rs
@@ -1,4 +1,22 @@
+//! Module for struct component [`EnableTextFiltering`].
+
 use bevy::prelude::Component;
 
+/// Whether the client has enabled text filtering or not.
+/// 
+/// If they do, the server should filter text before sending it to the client, and if they don't,
+/// the server can send unfiltered text to the client. The exact filtering method is up to you, but
+/// it should filter out innapropriate text.
+///
+/// *This is a component for player entities*.
+///
+/// The server should use this component when sending text packets to determine whether they should
+/// filter the text before sending it to the client.
+///
+/// ## Protocol representation
+/// See [`EnableTextFiltering::0`] for the protocol ID for each boolean value.
 #[derive(Component, Debug, Clone, Copy)]
-pub struct EnableTextFiltering(pub bool);
+pub struct EnableTextFiltering(
+
+    pub bool,
+);
