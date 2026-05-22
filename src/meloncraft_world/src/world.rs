15cbf5a7d8f1d22726574dfbefd82cdacdb828b7
dylancode <opensource@dylancode.dev>
2026-05-22 19:18:02 +0100

    document World

--- src/meloncraft_world/src/world.rs
+++ src/meloncraft_world/src/world.rs
@@ -1,7 +1,25 @@
+//! Module for struct [`World`].
+
 use std::collections::HashMap;
 use bevy::math::IVec2;
 use meloncraft_chunk::Chunk;
 
+/// A struct representing the world, which is a collection of chunks.
+///
+/// ## Representation
+/// These chunks are stored as a hashmap, where the key is the chunks position in the world.
+/// See [`World::chunks`] for more details on how the chunks are stored.
+///
+/// ### Chunk position
+/// Chunk positions in a [`World`] are calculated from a block position as follows:
+/// ```
+/// chunk_x = floor(block_x / 16)
+/// chunk_z = floor(block_z / 16)
+/// ```
+///
+/// ## state
+/// The world is not state by itself. You can use the `meloncraft_world_manager` crate if you want a
+/// ready-made world state storage.
 #[derive(Default)]
 pub struct World {
     chunks: HashMap<IVec2, Chunk>,
