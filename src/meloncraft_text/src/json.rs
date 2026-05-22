5a0ee871467d8fd1eec8148c30d94bfe9d993262
dylancode <opensource@dylancode.dev>
2026-05-22 17:20:30 +0100

    rename JsonText to SnbtText

--- src/meloncraft_text/src/json.rs
+++ src/meloncraft_text/src/json.rs
@@ -1,21 +1,21 @@
-//! Module for struct [`JsonText`].
+//! Module for struct [`SnbtText`].
 
-/// Container for Minecraft text, in the form of a JSON string.
+/// Container for Minecraft text, in the form of an SNBT (stringified NBT) value.
 ///
 /// The protocol has various packets where text, in the form of *components*, need to be sent. These
-/// can be sent either in the form of [`JsonText`], or, more commonly, as
+/// can be sent either in the form of [`SnbtText`], or, more commonly, as
 /// [`NbtText`](`crate::NbtText`).
-/// The main difference between the two is that [`JsonText`] is a stringified SNBT (similar to JSON,
+/// The main difference between the two is that [`SnbtText`] is a stringified SNBT (similar to JSON,
 /// but with some differences) and is usually used when *users* need to input text, while
 /// [`NbtText`] is a binary NBT structure encoded as a regular NBT tag value.
 ///
-/// Some packets use [`JsonText`], while some others use [`NbtText`]. Check the packet's
+/// Some packets use [`SnbtText`], while some others use [`NbtText`]. Check the packet's
 /// Meloncraft documentation, or the wiki for packets.
 ///
 /// The SNBT format for text components is explained at <https://minecraft.wiki/w/Text_component_format>.
 ///
-/// See [`JsonText::data`] for more information about the data this type should hold.
+/// See [`SnbtText::data`] for more information about the data this type should hold.
 #[derive(Debug, Clone)]
-pub struct JsonText {
+pub struct SnbtText {
     pub data: String,
 }
