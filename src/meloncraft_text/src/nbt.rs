94f6582d8961870eeee9099feb0b216dff350b82
dylancode <opensource@dylancode.dev>
2026-05-22 17:57:39 +0100

    document NbtText::Formatted

--- src/meloncraft_text/src/nbt.rs
+++ src/meloncraft_text/src/nbt.rs
@@ -13,6 +13,16 @@ pub enum NbtText {
     /// A plain string, without any formatting.
     /// This is the simplest form of text; it takes just one [`NbtString`] type.
     Plain(NbtString),
+
+    /// A formatted text, which can include various formatting options like colors, styles, and
+    /// additional text components.
+    /// This variant uses an [`NbtCompound`] to represent a more complex text component.
+    /// See the documentation for [`NbtCompound`] for info on the structure of a compound.
+    ///
+    /// The format of the compound is very similar to the [`SnbtText`](crate::snbt::SnbtText) struct,
+    /// but it is represented in binary NBT format instead of SNBT. More documentation may be
+    /// available for [`SnbtText`](crate::snbt::SnbtText) if you want to understand the structure of
+    /// text components.
     Formatted(NbtCompound),
 }
 
