8cec920eba11e9cf933887b1037a9a94d403043a
dylancode <opensource@dylancode.dev>
2026-05-22 11:01:47 +0100

    document Locale

--- src/meloncraft_player/src/locale.rs
+++ src/meloncraft_player/src/locale.rs
@@ -1,4 +1,17 @@
+//! Module for struct component [`Locale`].
+
 use bevy::prelude::Component;
 
+/// The locale (region and language information) of a player.
+/// This contains information about the player's region as well as their language preference.
+///
+/// *Component of a player entity*.
+///
+/// ## Packet references
+/// - Sent in the `ServerboundClientInformation` packet, during configuration.
+///
+/// ## Format
+/// The locale component has a string which is usually in the format of `language_REGION`, for
+/// example, `en_CA` for *English*, *Canada*.
 #[derive(Component, Debug, Clone)]
 pub struct Locale(pub String);
