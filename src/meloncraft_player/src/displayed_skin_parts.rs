b1ba4815066e1502be7fd45b9a2a52d41e5a20b4
dylancode <opensource@dylancode.dev>
2026-05-22 08:48:42 +0100

    document DisplayedSkinParts::cape

--- src/meloncraft_player/src/displayed_skin_parts.rs
+++ src/meloncraft_player/src/displayed_skin_parts.rs
@@ -11,9 +11,17 @@ use bevy::prelude::Component;
 /// In the protocol, this is represented as a `u8`, where each bit is a field in a bitmask.
 /// You can use `&` or `|` to set/get a specific field's value, from its binary representation.
 /// See this struct's fields for more the mask of each field.
+///
+/// Further reading: <https://minecraft.wiki/w/Java_Edition_protocol/Packets#Client_Information_(configuration)>.
 #[derive(Component, Debug, Clone, Eq, PartialEq)]
 #[expect(clippy::struct_excessive_bools, reason = "Not a state machine, but could be replaced with a packet bit set in the future.")]
 pub struct DisplayedSkinParts {
+
+    /// [`DisplayedSkinParts`] field boolean, where the value is:
+    /// - `true` if the player's cape is visible.
+    /// - `false` if the player's cape is not visible.
+    ///
+    /// **Protocol mask: `0x01`** (least significant bit).
     pub cape: bool,
     pub jacket: bool,
     pub left_sleeve: bool,
