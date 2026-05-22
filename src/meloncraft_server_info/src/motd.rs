0a2f32e0ebf30881f90cc4c3e65f8fc887b9c111
dylancode <opensource@dylancode.dev>
2026-05-22 13:16:45 +0100

    document Motd

--- src/meloncraft_server_info/src/motd.rs
+++ src/meloncraft_server_info/src/motd.rs
@@ -1,6 +1,16 @@
+//! Module for struct resource [`Motd`].
+
 use bevy::ecs::resource::Resource;
 use core::ops::{Deref, DerefMut};
 
+/// The server's *message of the day* (`MOTD`), as a `String`.
+///
+/// This can be thought of as the server *description*: it's the message that shows on the
+/// multiplayer server list screen, and is sent to clients in the `Status` packet by the
+/// `meloncraft_server_list` logic crate.
+///
+/// You should insert this resource to define the MOTD of your server, and you can change it at
+/// runtime to change the MOTD of your server on the fly.
 #[derive(Resource)]
 pub struct Motd(pub String);
 
