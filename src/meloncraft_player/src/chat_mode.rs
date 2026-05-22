f1350903959c29a334a8359f497b783dbf23ef12
dylancode <opensource@dylancode.dev>
2026-05-22 08:11:33 +0100

    document ChatMode

--- src/meloncraft_player/src/chat_mode.rs
+++ src/meloncraft_player/src/chat_mode.rs
@@ -1,4 +1,17 @@
+//! Module for component struct [`ChatColors`].
+
 use bevy::prelude::Component;
+
+/// Component storing the player's chat mode preferences. This is used to determine whether the
+/// player wants to see chat messages at all, or only command feedback, or all chat messages.
+///
+/// You should check this component before sending chat messages or command outputs to the user.
+///
+/// *Component for a player/client entity*.
+///
+/// See the variants' documentation for each mode and its implications.
+///
+/// Further reading: <https://minecraft.wiki/w/Java_Edition_protocol/Chat#Client_chat_mode>.
 
 #[derive(Component, Debug, Clone, Copy)]
 #[repr(i32)]
