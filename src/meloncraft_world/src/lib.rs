f91e2f8a65040a3ef63e29051f4aabea5108154b
dylancode <opensource@dylancode.dev>
2026-05-22 22:42:09 +0100

    document MeloncraftWorldPlugin

--- src/meloncraft_world/src/lib.rs
+++ src/meloncraft_world/src/lib.rs
@@ -1,15 +1,24 @@
 use bevy::app::{App, Plugin};
 
+use self::messages::{ChunkGenerated, ChunkRequest, GenerateChunk, SendChunk};
+
 pub mod world;
 pub mod messages;
 
+/// Plugin for registering messages related to a Minecraft world, e.g. chunk sending and generation.
+/// 
+/// ## Registered messages:
+/// - [`ChunkRequest`]
+/// - [`ChunkGenerated`]
+/// - [`SendChunk`]
+/// - [`GenerateChunk`]
 pub struct MeloncraftWorldPlugin;
 
 impl Plugin for MeloncraftWorldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_message::<messages::ChunkRequest>();
-        app.add_message::<messages::ChunkGenerated>();
-        app.add_message::<messages::SendChunk>();
-        app.add_message::<messages::GenerateChunk>();
+        app.add_message::<ChunkRequest>();
+        app.add_message::<ChunkGenerated>();
+        app.add_message::<SendChunk>();
+        app.add_message::<GenerateChunk>();
     }
 }
