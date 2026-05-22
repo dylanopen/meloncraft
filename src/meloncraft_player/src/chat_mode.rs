f7872678880043b34bad0abf4676708cbeadb197
dylancode <opensource@dylancode.dev>
2026-05-22 08:21:06 +0100

    document ChatMode::Full

--- src/meloncraft_player/src/chat_mode.rs
+++ src/meloncraft_player/src/chat_mode.rs
@@ -16,7 +16,37 @@ use bevy::prelude::Component;
 #[derive(Component, Debug, Clone, Copy)]
 #[repr(i32)]
 pub enum ChatMode {
-    Enabled,
+
+    /// [`ChatMode`] component variant on a player, indicating that the player would like to receive
+    /// *all* types of chat messages, including normal chat messages, system messages, and command
+    /// feedback.
+    ///
+    /// ## Enabled clientbound packets
+    /// The server should send these types of packets to a player with this chat mode:
+    /// - `ClientboundPlayerChatMessage`: Player-initiated chat messages, including the commands
+    ///   /say, /me, /msg, /tell, /w and /teammsg.
+    /// - `ClientboundDisguisedChatMessage`: Messages sent by non-players using the commands /say,
+    ///   /me, /msg, /tell, /w and /teammsg.
+    /// - `ClientboundSystemChatMessage`: Feedback from running a command, such as "Your game mode
+    ///   has been updated to creative." 
+    /// - `CilentboundSystemChatMessageOverlay`: Game state information that is displayed above
+    ///   the hot bar, such as "You may not rest now, the bed is too far away". 
+    ///
+    /// ## Disabled clientbound packets
+    /// None, the server should send all chat message types to a player with this chat mode.
+    ///
+    /// ## Enabled serverbound packets
+    /// The *client* will send these types of packets to the server when the player has this chat
+    /// mode:
+    /// - `ServerboundChatMessage`: When the player sends a normal chat message, such as "Hello
+    ///   world!".
+    /// - `ServerboundChatCommand`: When the player runs any Minecraft command, such as "/gamemode
+    ///   creative" or "/msg meloncrafter Hello world!".
+    ///
+    /// ## Disabled serverbound packets
+    /// None, the client should send all chat message types to the server when the player has this
+    /// chat mode.
+    Full,
     CommandsOnly,
     Hidden,
 }
@@ -25,7 +55,7 @@ impl TryFrom<i32> for ChatMode {
     type Error = ();
     fn try_from(value: i32) -> Result<Self, Self::Error> {
         return match value {
-            0 => Ok(ChatMode::Enabled),
+            0 => Ok(ChatMode::Full),
             1 => Ok(ChatMode::CommandsOnly),
             2 => Ok(ChatMode::Hidden),
             _ => Err(()),
