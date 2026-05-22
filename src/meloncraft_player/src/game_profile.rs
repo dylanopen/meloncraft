dccdcb8ab4e18c20d98ebd6e1c81bc96b784f534
dylancode <opensource@dylancode.dev>
2026-05-22 10:46:14 +0100

    document GameProfile::username

--- src/meloncraft_player/src/game_profile.rs
+++ src/meloncraft_player/src/game_profile.rs
@@ -27,6 +27,24 @@ pub struct GameProfile {
     /// This means it is recommended to identify players by their [`GameProfile::uuid`] rather than
     /// by their [`GameProfile::username`].
     pub uuid: Uuid,
+
+    /// The player's username, as a `String`.
+    /// This is the human-readable name they have chosen to go by.
+    ///
+    /// ## Uniqueness
+    /// In online-mode, usernames are guaranteed to be unique per-account (i.e. no two Microsoft
+    /// accounts can have the same Minecraft username).
+    /// However, in offline-mode, the username (and by derivative, the UUID) can just be chosen by
+    /// the client, and multiple users can attempt to join with the same username.
+    /// As this can cause many issues, most server softwares choose to reject any connections if the
+    /// username they try to log in as is already playing on the server. Or, some disconnect the
+    /// existing connection and accept the new one.
+    ///
+    /// ## Dynamic
+    /// A player can change their username, so if you use it to identify them, your server will break
+    /// for players who change their name.
+    /// It is highly recommended to use the [`GameProfile::uuid`] field to identify a player,
+    /// instead of their username.
     pub username: String,
 }
 
