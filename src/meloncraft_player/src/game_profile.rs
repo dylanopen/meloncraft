8ee7e3a56f22d3a605b876568c22b24e1f025739
dylancode <opensource@dylancode.dev>
2026-05-22 08:58:18 +0100

    document GameProfile

--- src/meloncraft_player/src/game_profile.rs
+++ src/meloncraft_player/src/game_profile.rs
@@ -3,6 +3,12 @@
 use crate::Uuid;
 use bevy::prelude::Component;
 
+/// Component storing a player's game profile, including their:
+/// - UUID ([`GameProfile::uuid`])
+/// - Username ([`GameProfile::username`])
+///
+/// This is used to store information **identifying** the player. It should be attached to a player
+/// entity as soon as you identify their username and UUID.
 #[derive(Component, Debug, Clone)]
 pub struct GameProfile {
     pub uuid: Uuid,
