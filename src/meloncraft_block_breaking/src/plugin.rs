3914b26da304695861990697e7a8023970bccf3f
dylancode <opensource@dylancode.dev>
2026-05-23 11:08:59 +0100

    create MeloncraftBlockBreakingPlugin

---
+++ src/meloncraft_block_breaking/src/plugin.rs
@@ -0,0 +1,12 @@
+//! Module for [`MeloncraftBlockBreakingPlugin`].
+
+use bevy::app::{App, Plugin};
+
+pub struct MeloncraftBlockBreakingPlugin;
+
+impl Plugin for MeloncraftBlockBreakingPlugin {
+    fn build(&self, app: &mut App) {
+        
+    }
+}
+
