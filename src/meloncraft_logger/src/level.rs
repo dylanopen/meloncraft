62b79beeb8f2e6e23c5dc929efed23243134ec07
dylancode <opensource@dylancode.dev>
2026-05-23 23:44:28 +0100

    create LogLevel::as_str

--- src/meloncraft_logger/src/level.rs
+++ src/meloncraft_logger/src/level.rs
@@ -10,3 +10,18 @@ pub enum LogLevel {
     Error,
 }
 
+impl LogLevel {
+    #[must_use]
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            LogLevel::Trace => return "TRACE",
+            LogLevel::Debug => return "DEBUG",
+            LogLevel::Info => return "INFO",
+            LogLevel::Command => return "COMMAND",
+            LogLevel::Chat => return "CHAT",
+            LogLevel::Warn => return "WARN",
+            LogLevel::Error => return "ERROR",
+        }
+    }
+}
+
