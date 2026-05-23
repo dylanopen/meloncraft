628e48ecf248967dcd9c00d655c1a8f66ab3ab2c
dylancode <opensource@dylancode.dev>
2026-05-23 20:17:09 +0100

    create MeloncraftDefaultCommandsPlugin

---
+++ src/meloncraft_default_commands/src/plugin.rs
@@ -0,0 +1,13 @@
+//! Module for [`MeloncraftDefaultCommandsPlugin`].
+
+use bevy::app::{App, Plugin};
+
+/// Plugin to register the default commands.
+/// This currently registers the following commands:
+pub struct MeloncraftDefaultCommandsPlugin;
+
+impl Plugin for MeloncraftDefaultCommandsPlugin {
+    fn build(&self, app: &mut App) {
+        
+    }
+}
