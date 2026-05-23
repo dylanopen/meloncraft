c24823a80186aec0c7900d640ae67dc491b2a06d
dylancode <opensource@dylancode.dev>
2026-05-23 20:20:01 +0100

    create command /teleport (empty for now)

---
+++ src/meloncraft_default_commands/src/teleport.rs
@@ -0,0 +1,9 @@
+//! Module for `/teleport` and `/tp` command.
+//! See [`cmd_teleport`].
+
+use bevy::ecs::message::MessageReader;
+use meloncraft_command::raw::RawCommand;
+
+pub fn cmd_teleport(mut raw_command_mr: MessageReader<RawCommand>) {
+
+}
