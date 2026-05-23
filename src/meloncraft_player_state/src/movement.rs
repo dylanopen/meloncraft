54a40f4d2c85bdc7a6cf2860ebae04350022f8ba
dylancode <opensource@dylancode.dev>
2026-05-23 13:31:52 +0100

    document fwd_player_moved

--- src/meloncraft_player_state/src/movement.rs
+++ src/meloncraft_player_state/src/movement.rs
@@ -4,6 +4,15 @@ use meloncraft_entity::position::EntityPosition;
 use meloncraft_packets::ServerboundSetPlayerPosition;
 use meloncraft_player::GameProfile;
 
+/// System for forwarding player movement packets as [`EntityMoved`] messages.
+///
+/// This system listens for [`ServerboundSetPlayerPosition`] packets, retrieves the player's old
+/// position from the ECS query, and writes an [`EntityMoved`] message with the old and
+/// new positions.
+///
+/// If the player does not have an existing position (i.e., they are new or their position has not
+/// been tracked yet), the system will use the new position from the packet as the old position in
+/// the [`EntityMoved`] message (i.e. [`EntityMoved::new_position`] will equal [`EntityMoved::old_position`]).
 pub fn fwd_player_moved(
     mut set_position_pr: MessageReader<ServerboundSetPlayerPosition>,
     mut player_moved_mw: MessageWriter<EntityMoved>,
