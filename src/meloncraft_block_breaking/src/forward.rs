0cb99a49efd57710741fd6828d0f2f0767915093
dylancode <opensource@dylancode.dev>
2026-05-23 11:23:40 +0100

    forward BlockBroken to SetBlock

---
+++ src/meloncraft_block_breaking/src/forward.rs
@@ -0,0 +1,18 @@
+//! Module for managing the chunk data in block breaking events.
+
+use bevy::ecs::message::{MessageReader, MessageWriter};
+use meloncraft_block::block::Block;
+use meloncraft_block::broken::BlockBroken;
+use meloncraft_block::set::SetBlock;
+
+pub fn set_broken_blocks(
+    mut block_broken_mr: MessageReader<BlockBroken>,
+    mut set_block_mw: MessageWriter<SetBlock>,
+) {
+    for block_broken in block_broken_mr.read() {
+        set_block_mw.write(SetBlock {
+            block_location: block_broken.block_location,
+            new_block: Block::new(0)
+        });
+    }
+}
--- src/meloncraft_block_breaking/src/plugin.rs
+++ src/meloncraft_block_breaking/src/plugin.rs
@@ -1,12 +1,14 @@
 //! Module for [`MeloncraftBlockBreakingPlugin`].
 
-use bevy::app::{App, Plugin};
+use bevy::app::{App, Plugin, Update};
+
+use crate::forward;
 
 pub struct MeloncraftBlockBreakingPlugin;
 
 impl Plugin for MeloncraftBlockBreakingPlugin {
     fn build(&self, app: &mut App) {
-        
+        app.add_systems(Update, forward::set_broken_blocks);
     }
 }
 
