9b83ce083e091bd3468eb67c03105d1acba5d579
dylancode <opensource@dylancode.dev>
2026-05-23 10:45:41 +0100

    create system generate_empty_overworld

--- src/meloncraft_world_manager/src/lib.rs
+++ src/meloncraft_world_manager/src/lib.rs
@@ -1,15 +1,16 @@
 pub mod marker;
 mod sender;
+mod generator;
 mod request_forwarding;
 
-use bevy::app::{App, Plugin, Startup, Update};
-use meloncraft_world::world::World;
-use crate::marker::Overworld;
+use bevy::app::{App, Plugin, PostStartup, Update};
 
 pub struct MeloncraftWorldManagerPlugin;
 
 impl Plugin for MeloncraftWorldManagerPlugin {
     fn build(&self, app: &mut App) {
+        app.add_systems(PostStartup, generator::generate_empty_overworld);
+
         app.add_systems(Update, sender::send_chunk);
         app.add_systems(Update, request_forwarding::send_requested_chunks);
         app.add_systems(Update, request_forwarding::send_generated_chunks);
