227b1fb54d26a2c39e389e27b1ea3494614bd955
dylancode <opensource@dylancode.dev>
2026-05-23 13:01:57 +0100

    delete mod entity/player

--- src/meloncraft_entity/src/player/mod.rs
+++
@@ -1,5 +0,0 @@
-//! Module containing messages (and later components) related *specifically* to **player**
-//! entities.
-//! Initialization plugin: [`MeloncraftPlayerEntityPlugin`](`plugin::MeloncraftPlayerEntityPlugin`).
-
-pub mod plugin;
