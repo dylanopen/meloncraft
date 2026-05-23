465f2f8c0c14a0016ccbe536e90606d6887d5ead
dylancode <opensource@dylancode.dev>
2026-05-23 14:56:04 +0100

    pub re-export crate command

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,4 @@ pub use meloncraft_packet_forwarding as packet_forwarding;
 pub use meloncraft_server_info as server_info;
 pub use meloncraft_block_breaking as block_breaking;
 pub use meloncraft_entity_state as entity_state;
+pub use meloncraft_command as command;
