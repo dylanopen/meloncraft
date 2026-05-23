ee7d866e59c524cc202d112c284c0cebf15d7540
dylancode <opensource@dylancode.dev>
2026-05-23 13:09:26 +0100

    create MeloncraftEntityStatePlugin

---
+++ src/meloncraft_entity_state/src/plugin.rs
@@ -0,0 +1,14 @@
+//! Module for [`MeloncraftEntityStatePlugin`].
+
+use bevy::app::{App, Plugin};
+
+pub struct MeloncraftEntityStatePlugin;
+
+/// Meloncraft plugin to handle entity events and store them as components on the Bevy Entity.
+/// For example, this responds to `EntityMoved` messages by storing the `EntityPosition` component
+/// on the entity, which can be queried later from the ECS.
+impl Plugin for MeloncraftEntityStatePlugin {
+    fn build(&self, app: &mut App) {
+
+    }
+}
