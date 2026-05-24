7c14165041ffc4272db71bb6873d5707746a4978
dylancode <opensource@dylancode.dev>
2026-05-24 17:41:43 +0100

    document insert_gamemode

--- src/meloncraft_player_state/src/gamemode.rs
+++ src/meloncraft_player_state/src/gamemode.rs
@@ -1,9 +1,13 @@
+//! Systems for managing a player's [`GameMode`].
+
 use bevy::ecs::entity::Entity;
 use bevy::ecs::query::Added;
 use bevy::ecs::system::{Commands, Query};
 use meloncraft_core::GameMode;
 use meloncraft_player::PlayerMarker;
 
+/// Inserts a [`GameMode`] component with the default value of `Survival` for each player that has
+/// just loaded in.
 pub fn insert_gamemode(
     mut commands: Commands,
     added_player_q: Query<Entity, Added<PlayerMarker>>,
