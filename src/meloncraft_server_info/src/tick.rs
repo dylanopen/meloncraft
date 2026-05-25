35a7f9ba11f845c750f67e82d48c307830b4d8f3
dylancode <opensource@dylancode.dev>
2026-05-25 16:58:34 +0100

    create resource TickingFrozen

--- src/meloncraft_server_info/src/tick.rs
+++ src/meloncraft_server_info/src/tick.rs
@@ -1,4 +1,4 @@
-//! Module for resource struct [`TickRate`].
+//! Module for resource structs [`TickRate`] and [`TickingFrozen`].
 
 use bevy::ecs::resource::Resource;
 
@@ -10,11 +10,23 @@ use bevy::ecs::resource::Resource;
 /// `ClientboundSetTickingState` packet wants
 /// (see <https://minecraft.wiki/w/Java_Edition_protocol/Packets#Set_Ticking_State>).
 #[derive(Resource, Debug, Clone, Copy)]
-
 pub struct TickRate(
 
     /// The target tick rate, in ticks per second (TPS), as an `f32`.
     /// Default: `20.0`, meaning 20 ticks occur each second (0.05 seconds per tick).
     pub f32,
+);
+
+
+/// Resource storing whether or not the server's ticking state is frozen.
+///
+/// See [`TickingFrozen::0`] for its field.
+#[derive(Resource, Debug, Clone, Copy)]
+pub struct TickingFrozen(
+
+    /// Whether or not the server's ticking state is frozen.
+    /// - `true` if the server's ticking is frozen.
+    /// - `false` if it is not.
+    pub bool,
 );
 
