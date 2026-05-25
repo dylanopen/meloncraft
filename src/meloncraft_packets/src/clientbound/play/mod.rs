f0e8d8ff3abf1c91ba0c8ede22075ea9db67a655
dylancode <opensource@dylancode.dev>
2026-05-25 20:21:48 +0100

    create ClientboundSetBorderWarningDistance

--- src/meloncraft_packets/src/clientbound/play/mod.rs
+++ src/meloncraft_packets/src/clientbound/play/mod.rs
@@ -42,6 +42,9 @@ pub use set_border_size::ClientboundSetBorderSize;
 mod set_border_warning_delay;
 pub use set_border_warning_delay::ClientboundSetBorderWarningDelay;
 
+mod set_border_warning_distance;
+pub use set_border_warning_distance::ClientboundSetBorderWarningDistance;
+
 pub fn register_clientbound_play_packets(app: &mut App) {
     use crate::clientbound_messenger::fwd;
     use bevy::app::PostUpdate;
@@ -87,4 +90,7 @@ pub fn register_clientbound_play_packets(app: &mut App) {
 
     app.add_message::<ClientboundSetBorderWarningDelay>();
     app.add_systems(PostUpdate, fwd::<ClientboundSetBorderWarningDelay>);
+
+    app.add_message::<ClientboundSetBorderWarningDistance>();
+    app.add_systems(PostUpdate, fwd::<ClientboundSetBorderWarningDistance>);
 }
---
+++ src/meloncraft_packets/src/clientbound/play/set_border_warning_distance.rs
@@ -0,0 +1,43 @@
+use bevy::ecs::message::Message;
+use bevy::prelude::Entity;
+use meloncraft_client::connection_state::ConnectionState;
+use meloncraft_server_info::world_border::WorldBorderWarningDistance;
+use crate::network_messages::ClientboundNetworkPacket;
+use meloncraft_protocol_types::ProtocolType as _;
+use crate::clientbound_packet::ClientboundPacket;
+
+/// Set the minimum distance a player needs to be to the world border, until the red outline on
+/// their screen, warning them that they are too close to the world border.
+/// See [`WorldBorderWarningDistance`].
+#[derive(Message, Debug, Clone)]
+pub struct ClientboundSetBorderWarningDistance {
+    pub client: Entity,
+
+/// Set the minimum distance a player needs to be to the world border, until the red outline on
+/// their screen, warning them that they are too close to the world border.
+/// See [`WorldBorderWarningDistance`].
+    pub warning_distance: WorldBorderWarningDistance,
+}
+
+impl ClientboundPacket for ClientboundSetBorderWarningDistance {
+    fn id() -> i32 {
+        return 0x5A
+    }
+
+    fn state() -> ConnectionState {
+        return ConnectionState::Play
+    }
+
+    fn serialize(&self) -> Option<ClientboundNetworkPacket> {
+        let mut data = Vec::new();
+
+        data.extend(self.warning_distance.0.net_serialize());
+
+        return Some(ClientboundNetworkPacket {
+            client: self.client,
+            id: Self::id(),
+            data,
+        })
+    }
+}
+
