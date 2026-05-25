a9a8788c9af3e923067ca442a771ba6af6bd7661
dylancode <opensource@dylancode.dev>
2026-05-25 12:12:48 +0100

    document difficulty protocol ids

--- src/meloncraft_player/src/difficulty.rs
+++ src/meloncraft_player/src/difficulty.rs
@@ -4,24 +4,28 @@
 /// `Peaceful`, `Easy`, `Normal` and `Hard`.
 ///
 /// See the variants' documentation or the Minecraft wiki for information on what the different
-/// difficulties do.
+/// difficulties do, as well as their protocol IDs.
 /// <https://minecraft.wiki/w/Difficulty>.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Difficulty {
 
     /// **Peaceful** [`Difficulty`].
+    /// Protocol ID: `0`.
     /// See <https://minecraft.wiki/w/Difficulty#Peaceful> for more information about peaceful mode.
     Peaceful,
 
     /// **Easy** [`Difficulty`].
+    /// Protocol ID: `1`.
     /// See <https://minecraft.wiki/w/Difficulty#Easy> for more information about easy mode.
     Easy,
 
     /// **Normal** [`Difficulty`].
+    /// Protocol ID: `2`.
     /// See <https://minecraft.wiki/w/Difficulty#Normal> for more information about normal mode.
     Normal,
 
     /// **Hard** [`Difficulty`].
+    /// Protocol ID: `3`.
     /// See <https://minecraft.wiki/w/Difficulty#Hard> for more information about hard mode.
     Hard,
 }
