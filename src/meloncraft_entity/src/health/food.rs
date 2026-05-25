11ecef3b8de10fe8ebf585f0477dfdce7bc3e941
dylancode <opensource@dylancode.dev>
2026-05-25 23:58:19 +0100

    create component FoodSaturation

--- src/meloncraft_entity/src/health/food.rs
+++ src/meloncraft_entity/src/health/food.rs
@@ -1,4 +1,4 @@
-//! Module for component struct [`FoodHealth`].
+//! Module for component structs [`FoodHealth`] and [`FoodSaturation`].
 
 use bevy::ecs::component::Component;
 
@@ -6,3 +6,15 @@ use bevy::ecs::component::Component;
 /// For a player, this ranges from 0 (starving) to 20 (completely full).
 #[derive(Component, Debug, Clone, Copy)]
 pub struct FoodHealth(pub i32);
+
+/// The saturation of the entity.
+///
+/// Food saturation acts as a food “overcharge”. Food values will not decrease while the
+/// saturation is over zero. New players logging in or respawning automatically get a
+/// saturation of 5.0. Eating food increases the saturation as well as the food bar.
+///
+/// Seems to vary from 0.0 to 5.0 in integer increments.
+///
+/// <https://minecraft.wiki/w/Java_Edition_protocol/Packets#Set_Health>.
+#[derive(Component, Debug, Clone, Copy)]
+pub struct FoodSaturation(pub i32);
