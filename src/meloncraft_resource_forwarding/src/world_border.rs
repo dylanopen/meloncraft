f515a303e31dd3e778a30b6a7100e33ba1bbfabd
dylancode <opensource@dylancode.dev>
2026-05-25 21:40:37 +0100

    WorldBorderWarningDistance forwarder

--- src/meloncraft_resource_forwarding/src/world_border.rs
+++ src/meloncraft_resource_forwarding/src/world_border.rs
@@ -5,9 +5,9 @@ use bevy::ecs::entity::Entity;
 use bevy::ecs::message::MessageWriter;
 use bevy::ecs::query::{Added, With};
 use bevy::ecs::system::{Query, Res};
-use meloncraft_packets::{ClientboundSetBorderCenter, ClientboundSetBorderSize, ClientboundSetBorderWarningDelay};
+use meloncraft_packets::{ClientboundSetBorderCenter, ClientboundSetBorderSize, ClientboundSetBorderWarningDelay, ClientboundSetBorderWarningDistance};
 use meloncraft_player::PlayerMarker;
-use meloncraft_server_info::world_border::{WorldBorderCenter, WorldBorderDiameter, WorldBorderWarningDelay};
+use meloncraft_server_info::world_border::{WorldBorderCenter, WorldBorderDiameter, WorldBorderWarningDelay, WorldBorderWarningDistance};
 
 pub fn send_world_border_center_on_join(
     new_player_q: Query<Entity, Added<PlayerMarker>>,
@@ -88,6 +88,34 @@ pub fn send_world_border_warning_delay_on_change(
         set_border_warning_delay_pw.write(ClientboundSetBorderWarningDelay {
             client,
             warning_delay: world_border_warning_delay.clone(),
+        });
+    }
+}
+
+
+pub fn send_world_border_warning_distance_on_join(
+    new_player_q: Query<Entity, Added<PlayerMarker>>,
+    world_border_warning_distance: Res<WorldBorderWarningDistance>,
+    mut set_border_warning_distance_pw: MessageWriter<ClientboundSetBorderWarningDistance>,
+) {
+    for client in new_player_q {
+        set_border_warning_distance_pw.write(ClientboundSetBorderWarningDistance {
+            client,
+            warning_distance: world_border_warning_distance.clone(),
+        });
+    }
+}
+
+pub fn send_world_border_warning_distance_on_change(
+    player_q: Query<Entity, With<PlayerMarker>>,
+    world_border_warning_distance: Res<WorldBorderWarningDistance>,
+    mut set_border_warning_distance_pw: MessageWriter<ClientboundSetBorderWarningDistance>,
+) {
+    if !world_border_warning_distance.is_changed() { return; }
+    for client in player_q {
+        set_border_warning_distance_pw.write(ClientboundSetBorderWarningDistance {
+            client,
+            warning_distance: world_border_warning_distance.clone(),
         });
     }
 }
