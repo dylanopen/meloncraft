0c7f848afb90d069eb20de027642c6adcf0451b9
dylancode <opensource@dylancode.dev>
2026-05-25 12:11:39 +0100

    move Difficulty from core to player

--- src/meloncraft_core/src/lib.rs
+++ src/meloncraft_core/src/lib.rs
@@ -23,7 +23,6 @@ pub mod resource_pack_load_result;
 pub mod gamemode;
 pub mod demo_event;
 pub mod weather_intensity;
-pub mod difficulty;
 pub mod game_event;
 
 pub use disconnect_report::DisconnectReport;
