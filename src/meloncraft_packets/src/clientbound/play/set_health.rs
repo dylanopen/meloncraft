32617b744936699f249940fe76625e3930d30686
dylancode <opensource@dylancode.dev>
2026-05-26 09:40:55 +0100

    create ClientboundSetHealth

---
+++ src/meloncraft_packets/src/clientbound/play/set_health.rs
@@ -0,0 +1,41 @@
+use bevy::ecs::message::Message;
+use bevy::prelude::Entity;
+use meloncraft_client::connection_state::ConnectionState;
+use meloncraft_entity::health::current::CurrentHealth;
+use meloncraft_entity::health::food::{FoodHealth, FoodSaturation};
+use crate::network_messages::ClientboundNetworkPacket;
+use meloncraft_protocol_types::{ProtocolType as _, VarInt};
+use crate::clientbound_packet::ClientboundPacket;
+
+#[derive(Message, Debug, Clone)]
+pub struct ClientboundSetHealth {
+    pub client: Entity,
+    pub current: CurrentHealth,
+    pub food: FoodHealth,
+    pub saturation: FoodSaturation,
+}
+
+impl ClientboundPacket for ClientboundSetHealth {
+    fn id() -> i32 {
+        return 0x66
+    }
+
+    fn state() -> ConnectionState {
+        return ConnectionState::Play
+    }
+
+    fn serialize(&self) -> Option<ClientboundNetworkPacket> {
+        let mut data = Vec::new();
+
+        data.extend(self.current.0.net_serialize());
+        data.extend(VarInt(self.food.0).net_serialize());
+        data.extend(self.saturation.0.net_serialize());
+
+        return Some(ClientboundNetworkPacket {
+            client: self.client,
+            id: Self::id(),
+            data,
+        })
+    }
+}
+
