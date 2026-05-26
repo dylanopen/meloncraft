7be0da9183fc73780762ea5e42176c338c0b1ca2
dylancode <opensource@dylancode.dev>
2026-05-26 17:21:53 +0100

    test experience::total_to_level

--- src/meloncraft_player/src/experience.rs
+++ src/meloncraft_player/src/experience.rs
@@ -17,5 +17,20 @@ pub fn total_to_level(total: i32) -> f32 {
     return 325.0/18.0 + (2.0/9.0 * (total - 54215.0/72.0)).sqrt();
 }
 
-
+#[cfg(test)]
+mod tests {
+    #[expect(clippy::float_cmp, reason = "The float should always be an exact value")]
+    #[test]
+    fn total_to_exact_level() {
+        use super::*;
+        assert_eq!(total_to_level(7), 1.0);
+        assert_eq!(total_to_level(187), 11.0);
+        assert_eq!(total_to_level(352), 16.0);
+        assert_eq!(total_to_level(394), 17.0);
+        assert_eq!(total_to_level(828), 24.0);
+        assert_eq!(total_to_level(1507), 31.0);
+        assert_eq!(total_to_level(1628), 32.0);
+        // assert_eq!(total_to_level(10540), 65.0); // this fails for some reason, likely a float division error?
+    }
+}
 
