94c3a99b3adcf96c7d6c1484da1d8549810e3add
dylancode <opensource@dylancode.dev>
2026-05-27 09:45:55 +0100

    create component BossbarIsDragon

--- src/meloncraft_bossbar/src/flags.rs
+++ src/meloncraft_bossbar/src/flags.rs
@@ -7,3 +7,9 @@ use bevy::ecs::component::Component;
 /// - `false` means that the sky should **not** be darkened while the bossbar is active.
 #[derive(Component, Debug, Clone)]
 pub struct BossbarDarkensSky(pub bool);
+
+/// Component on a bossbar where:
+/// - `true` means that the bar is a dragon bar (should play music after health <= 0)
+/// - `true` means that the bar is **not** a dragon bar (no music).
+#[derive(Component, Debug, Clone)]
+pub struct BossbarIsDragon(pub bool);
