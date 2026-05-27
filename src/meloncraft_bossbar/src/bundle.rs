50232fb3940b328ba5918374b482d4608a2cb101
dylancode <opensource@dylancode.dev>
2026-05-27 09:50:57 +0100

    add all new components to BossbarBundle

--- src/meloncraft_bossbar/src/bundle.rs
+++ src/meloncraft_bossbar/src/bundle.rs
@@ -3,10 +3,20 @@
 use bevy::ecs::bundle::Bundle;
 use meloncraft_entity::health::current::CurrentHealth;
 
+use crate::color::BossbarColor;
+use crate::division::BossbarDivision;
+use crate::flags::{BossbarCreatesFog, BossbarDarkensSky, BossbarIsDragon};
 use crate::marker::BossbarMarker;
+use crate::title::BossbarTitle;
 
 #[derive(Bundle, Debug, Clone)]
 pub struct BossbarBundle {
     pub marker: BossbarMarker,
     pub health: CurrentHealth,
+    pub title: BossbarTitle,
+    pub color: BossbarColor,
+    pub division: BossbarDivision,
+    pub darkens_sky: BossbarDarkensSky,
+    pub is_dragon: BossbarIsDragon,
+    pub creates_fog: BossbarCreatesFog,
 }
