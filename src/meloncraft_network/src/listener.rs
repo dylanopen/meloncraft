41cbad26d41b66547feab9c7b0dd937f795c0703
dylancode <opensource@dylancode.dev>
2026-05-27 15:42:57 +0100

    listen for new connections

--- src/meloncraft_network/src/listener.rs
+++ src/meloncraft_network/src/listener.rs
@@ -1,6 +1,24 @@
 use std::net::TcpListener;
 
 use bevy::ecs::resource::Resource;
+use bevy::ecs::system::Res;
+use meloncraft_logger::{debuglog, errorlog};
 
 #[derive(Resource, Debug)]
 pub struct NewClientListener(pub TcpListener);
+
+pub fn handle_new_clients(listener: Res<NewClientListener>) {
+    for stream in listener.0.incoming() {
+        let Ok(stream) = stream else {
+            errorlog!(
+                "Failed to get incoming TcpStream from client, ignoring connection. Error: {}",
+                stream.unwrap_err()
+            );
+            continue;
+        };
+        debuglog!(
+            "TCP connection established with client IP {}",
+            stream.peer_addr().unwrap()
+        );
+    }
+}
--- src/meloncraft_network/src/plugin.rs
+++ src/meloncraft_network/src/plugin.rs
@@ -1,7 +1,16 @@
-use bevy::app::{App, Plugin};
+use std::net::TcpListener;
+
+use bevy::app::{App, Plugin, PreUpdate};
+
+use crate::listener::{self, NewClientListener};
 
 pub struct MeloncraftNetworkPlugin;
 
 impl Plugin for MeloncraftNetworkPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.insert_resource(NewClientListener(
+            TcpListener::bind("127.0.0.1:25565").unwrap(),
+        ));
+        app.add_systems(PreUpdate, listener::handle_new_clients);
+    }
 }
