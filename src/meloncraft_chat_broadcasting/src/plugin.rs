ee79a2190cf023f0f1d740d7bb84c605fdb8066f
dylancode <opensource@dylancode.dev>
2026-05-28 22:27:48 +0100

    broadcast received player chat messages

--- src/meloncraft_chat_broadcasting/src/plugin.rs
+++ src/meloncraft_chat_broadcasting/src/plugin.rs
@@ -6,5 +6,6 @@ pub struct MeloncraftChatBroadcastingPlugin;
 
 impl Plugin for MeloncraftChatBroadcastingPlugin {
     fn build(&self, app: &mut App) {
+        app.add_systems(Update, send::send_player_chat);
     }
 }
