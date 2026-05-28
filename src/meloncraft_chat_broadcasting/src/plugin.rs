cfffbc6e7765156fabc615c316fc584859cbde3b
dylancode <opensource@dylancode.dev>
2026-05-28 22:12:44 +0100

    create MeloncraftChatBroadcastingPlugin

---
+++ src/meloncraft_chat_broadcasting/src/plugin.rs
@@ -0,0 +1,10 @@
+use bevy::app::{App, Plugin, Update};
+
+use crate::send;
+
+pub struct MeloncraftChatBroadcastingPlugin;
+
+impl Plugin for MeloncraftChatBroadcastingPlugin {
+    fn build(&self, app: &mut App) {
+    }
+}
