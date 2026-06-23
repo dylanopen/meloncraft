d641ae2c045296d80050cf4a2cccf112386e6602
dylancode <opensource@dylancode.dev>
2026-06-23 21:39:33 +0100

    create SendTitleMessage

--- src/meloncraft_chat/src/send.rs
+++ src/meloncraft_chat/src/send.rs
@@ -1,4 +1,4 @@
-//! Module for message struct [`SendChatMessage`].
+//! Module for message structs [`SendChatMessage`] and [`SendTitleMessage`].
 
 use bevy::ecs::entity::Entity;
 use bevy::ecs::message::Message;
@@ -10,6 +10,23 @@ use meloncraft_text::NbtText;
 pub struct SendChatMessage {
     /// A list of player entities that should receive this message.
     /// Listening systems should send a chat packet to all these clients.
+    /// This may contain every entity on the server.
+    pub receivers: Vec<Entity>,
+
+    /// The actual message itself. This is formatted text, in the form of an `NbtCompound` or
+    /// `NbtString`, see [`NbtText`] for details.
+    pub message: NbtText,
+}
+
+/// Message indicating that the server has sent a title message to a list of clients.
+/// See the fields for more details.
+///
+/// Title messages show up in the centre of the client's screen and shortly disappear.
+/// Sending one while a message is still visible will overwrite the old title message.
+#[derive(Message, Debug, Clone)]
+pub struct SendTitleMessage {
+    /// A list of player entities that should receive this message.
+    /// Listening systems should send a set title text  packet to all these clients.
     /// This may contain every entity on the server.
     pub receivers: Vec<Entity>,
 
--- src/meloncraft_packet_forwarding/src/chat_message.rs
+++ src/meloncraft_packet_forwarding/src/chat_message.rs
@@ -1,7 +1,7 @@
 use bevy::ecs::message::{MessageReader, MessageWriter};
-use meloncraft_chat::send::SendChatMessage;
+use meloncraft_chat::send::{SendChatMessage, SendTitleMessage};
 use meloncraft_chat::sent::PlayerSentChatMessage;
-use meloncraft_packets::{ClientboundSystemChat, ServerboundChat};
+use meloncraft_packets::{ClientboundSetTitleText, ClientboundSystemChat, ServerboundChat};
 
 pub fn fwd_player_sent(
     mut chat_pr: MessageReader<ServerboundChat>,
@@ -26,6 +26,20 @@ pub fn fwd_send_chat(
                 client: receiver,
                 message: send_chat.message.clone(),
                 overlay: false,
+            });
+        }
+    }
+}
+
+pub fn fwd_send_title(
+    mut send_title_mr: MessageReader<SendTitleMessage>,
+    mut set_title_text_pw: MessageWriter<ClientboundSetTitleText>,
+) {
+    for send_chat in send_title_mr.read() {
+        for receiver in send_chat.receivers.clone() {
+            set_title_text_pw.write(ClientboundSetTitleText {
+                client: receiver,
+                title: send_chat.message.clone(),
             });
         }
     }
